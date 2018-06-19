c7816deaee282f396182cade8b566c73e7256f04
lucas.reliefapps <lucas.reliefapps@gmail.com>
2018-06-19 12:07:03 +0200

    add doc swg in donorController

--- src/ProjectBundle/Controller/SectorController.php
+++ src/ProjectBundle/Controller/SectorController.php
@@ -11,11 +11,25 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\Annotations as Rest;
 
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
+
 class SectorController extends Controller
 {
 
     /**
      * @Rest\Get("/sectors", name="get_all_sectors")
+     *
+     * @SWG\Tag(name="Sectors")
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="All Sectors",
+     *     @SWG\Schema(
+     *          type="array",
+     *          @SWG\Items(ref=@Model(type=Sector::class))
+     *     )
+     * )
      */
     public function getAllAction(Request $request)
     {
@@ -29,6 +43,27 @@ class SectorController extends Controller
 
     /**
      * @Rest\Put("/sectors", name="add_sector")
+     *
+     * @SWG\Tag(name="Sectors")
+     *
+     * @SWG\Parameter(
+     *     name="sector",
+     *     in="body",
+     *     required=true,
+     *     @Model(type=Sector::class, groups={"FullDonor"})
+     * )
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Sector created",
+     *     @Model(type=Sector::class)
+     * )
+     *
+     * @SWG\Response(
+     *     response=400,
+     *     description="BAD_REQUEST"
+     * )
+     *
      * @param Request $request
      * @return Response
      */
@@ -53,6 +88,26 @@ class SectorController extends Controller
     /**
      * @Rest\Post("/sectors/{id}", name="update_sector")
      *
+     * @SWG\Tag(name="Sectors")
+     *
+     * @SWG\Parameter(
+     *     name="Sector",
+     *     in="body",
+     *     required=true,
+     *     @Model(type=Sector::class, groups={"FullDonor"})
+     * )
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Sector updated",
+     *     @Model(type=Sector::class)
+     * )
+     *
+     * @SWG\Response(
+     *     response=400,
+     *     description="BAD_REQUEST"
+     * )
+     *
      * @param Request $request
      * @return Response
      */
@@ -76,6 +131,14 @@ class SectorController extends Controller
 
     /**
      * @Rest\Get("/sectors/{id}", name="show_sector")
+     *
+     * @SWG\Tag(name="Sectors")
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Sector asked",
+     *     @Model(type=Sector::class)
+     * )
      *
      * @param Sector $sector
      * @return Response
