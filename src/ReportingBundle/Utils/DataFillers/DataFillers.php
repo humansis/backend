6a89dfd904aa2b29ed1f1d7c09ec94d22555f38d
JuliePetitjean <julie.reliefapps@gmail.com>
2018-06-29 14:43:39 +0200

    add function fill in default dataFillers

--- src/ReportingBundle/Utils/DataFillers/DataFillers.php
+++ src/ReportingBundle/Utils/DataFillers/DataFillers.php
@@ -3,6 +3,9 @@
 namespace ReportingBundle\Utils\DataFillers;
 
 use ReportingBundle\Utils\DataFillers\DataFillerInterface;
+use ReportingBundle\Utils\DataFillers\DataFillersCountry;
+use ReportingBundle\Utils\DataFillers\DataFillersProject;
+use ReportingBundle\Utils\DataFillers\DataFillersDistribution;
 use ReportingBundle\Utils\Model\IndicatorInterface;
 
 use Doctrine\ORM\EntityManager;
@@ -19,6 +22,31 @@ class DataFillers implements DataFillerInterface
 
     public function fill(IndicatorInterface $indicator)
     {
+
+        if(preg_match("#^EU?_C#", $indicator->getCode())) 
+        {
+            if(is_callable(array(new DataFillersCountry($this->em), $indicator->getCode())))
+            {
+                return call_user_func_array([new DataFillersCountry($this->em), $indicator->getCode()], []);
+        
+            }
+        }
+
+        if(preg_match("#^EU?_P#", $indicator->getCode())) 
+        {
+            if(is_callable(array(new DataFillersProject($this->em), $indicator->getCode())))
+            {
+                return call_user_func_array([new DataFillersProject($this->em), $indicator->getCode()], []);
+            }
+        }
+
+        if(preg_match("#^EU?_D#", $indicator->getCode())) 
+        {
+            if(is_callable(array(new DataFillersDistribution($this->em), $indicator->getCode())))
+            {
+                return call_user_func_array([new DataFillersDistribution($this->em), $indicator->getCode()], []);
+            }
+        }
 
     }
 
