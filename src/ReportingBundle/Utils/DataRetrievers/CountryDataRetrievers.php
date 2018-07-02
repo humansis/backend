238cb7dc4b87e25bd35a36246686587e2c06c6f8
JuliePetitjean <julie.reliefapps@gmail.com>
2018-07-02 15:02:48 +0200

    add all request in dataRetrievers country

--- src/ReportingBundle/Utils/DataRetrievers/CountryDataRetrievers.php
+++ src/ReportingBundle/Utils/DataRetrievers/CountryDataRetrievers.php
@@ -19,6 +19,103 @@ class CountryDataRetrievers implements DataRetrieverInterface
         $this->reportingCountry = $em->getRepository(ReportingCountry::class);
     }
 
+    /**
+     * Get total of household by country
+     */
+    public function BMS_Country_TH(array $filters)
+    {
+       $qb = $this->reportingCountry->createQueryBuilder('rc')
+                                  ->leftjoin('rc.value', 'rv')
+                                  ->leftjoin('rc.indicator', 'ri')
+                                  ->where('ri.code = :code')
+                                    ->setParameter('code', 'BMS_Country_TH')
+                                  ->andWhere('rc.country = :country')
+                                    ->setParameter('country', $filters['country'])
+                                  ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', 'rv.creationDate AS date');
+        return $qb->getQuery()->getArrayResult();
+    }
+
+
+    /**
+     * Get total of beneficiaries by country
+     */
+    public function BMS_Country_TB(array $filters)
+    {
+       $qb = $this->reportingCountry->createQueryBuilder('rc')
+                                  ->leftjoin('rc.value', 'rv')
+                                  ->leftjoin('rc.indicator', 'ri')
+                                  ->where('ri.code = :code')
+                                    ->setParameter('code', 'BMS_Country_TB')
+                                  ->andWhere('rc.country = :country')
+                                    ->setParameter('country', $filters['country'])
+                                  ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', 'rv.creationDate AS date');
+        return $qb->getQuery()->getArrayResult();
+    }
+
+    /**
+     * Get total of active projects by country
+     */
+    public function BMS_Country_AP(array $filters)
+    {
+       $qb = $this->reportingCountry->createQueryBuilder('rc')
+                                  ->leftjoin('rc.value', 'rv')
+                                  ->leftjoin('rc.indicator', 'ri')
+                                  ->where('ri.code = :code')
+                                    ->setParameter('code', 'BMS_Country_AP')
+                                  ->andWhere('rc.country = :country')
+                                    ->setParameter('country', $filters['country'])
+                                  ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', 'rv.creationDate AS date');
+        return $qb->getQuery()->getArrayResult();
+    }
+
+    /**
+     * Get total funding by country
+     */
+    public function BMS_Country_TF(array $filters)
+    {
+       $qb = $this->reportingCountry->createQueryBuilder('rc')
+                                  ->leftjoin('rc.value', 'rv')
+                                  ->leftjoin('rc.indicator', 'ri')
+                                  ->where('ri.code = :code')
+                                    ->setParameter('code', 'BMS_Country_TF')
+                                  ->andWhere('rc.country = :country')
+                                    ->setParameter('country', $filters['country'])
+                                  ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', 'rv.creationDate AS date');
+        return $qb->getQuery()->getArrayResult();
+    }
+
+    /**
+     * Get total of enrolled beneficiaries by country
+     */
+    public function BMS_Country_EB(array $filters)
+    {
+       $qb = $this->reportingCountry->createQueryBuilder('rc')
+                                  ->leftjoin('rc.value', 'rv')
+                                  ->leftjoin('rc.indicator', 'ri')
+                                  ->where('ri.code = :code')
+                                    ->setParameter('code', 'BMS_Country_EB')
+                                  ->andWhere('rc.country = :country')
+                                    ->setParameter('country', $filters['country'])
+                                  ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', 'rv.creationDate AS date');
+        return $qb->getQuery()->getArrayResult();
+    }
+
+    /**
+     * Get total number of distributions by country
+     */
+    public function BMS_Country_TND(array $filters)
+    {
+       $qb = $this->reportingCountry->createQueryBuilder('rc')
+                                  ->leftjoin('rc.value', 'rv')
+                                  ->leftjoin('rc.indicator', 'ri')
+                                  ->where('ri.code = :code')
+                                    ->setParameter('code', 'BMS_Country_TND')
+                                  ->andWhere('rc.country = :country')
+                                    ->setParameter('country', $filters['country'])
+                                  ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', 'rv.creationDate AS date');
+        return $qb->getQuery()->getArrayResult();
+    }
+
 
 
 }
