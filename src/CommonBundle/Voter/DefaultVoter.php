9d6b49e4cba930e6b552e054384064d0afe8eb8f
lucas.reliefapps <lucas.reliefapps@gmail.com>
2018-07-06 09:07:15 +0200

    add first VOTER to only check if the user has the role wanted

--- src/CommonBundle/Voter/DefaultVoter.php
+++ src/CommonBundle/Voter/DefaultVoter.php
@@ -6,9 +6,23 @@ namespace CommonBundle\Voter;
 
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\Role\RoleHierarchy;
+use UserBundle\Entity\User;
 
 class DefaultVoter extends Voter
 {
+
+    /**
+     * @var RoleHierarchy $roleHierarchy
+     */
+    private $roleHierarchy;
+
+    public function __construct(RoleHierarchy $roleHierarchy)
+    {
+        $this->roleHierarchy = $roleHierarchy;
+    }
 
     /**
      * Determines if the attribute and subject are supported by this voter.
@@ -20,7 +34,7 @@ class DefaultVoter extends Voter
      */
     protected function supports($attribute, $subject)
     {
-        // TODO: Implement supports() method.
+        return true;
     }
 
     /**
@@ -35,6 +49,46 @@ class DefaultVoter extends Voter
      */
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
-        // TODO: Implement voteOnAttribute() method.
+        $user = $token->getUser();
+        if (!$user instanceof User)
+        {
+            return VoterInterface::ACCESS_DENIED;
+        }
+        /**
+         * @var User $user
+         */
+        return $this->canDoTask($user->getRoles(), $attribute);
+    }
+
+    /**
+     * @param array $myRoles
+     * @param string $attribute
+     * @return bool
+     */
+    protected function canDoTask(array $myRoles, string $attribute)
+    {
+        $myArrayRoles = $this->getMyReachableRoles($myRoles);
+
+        foreach($myArrayRoles as $role)
+        {
+            if ($role->getRole() === strval($attribute))
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array $my_roles
+     * @return array|\Symfony\Component\Security\Core\Role\RoleInterface[]
+     */
+    protected function getMyReachableRoles(array $my_roles)
+    {
+        $arrayRoles = [];
+        foreach ($my_roles as $role)
+        {
+            $arrayRoles[] = new Role($role);
+        }
+        return $this->roleHierarchy->getReachableRoles($arrayRoles);
     }
 }
