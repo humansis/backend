629295ae08100df03ac08240c4a12d49e49fd28d
lucas.reliefapps <lucas.reliefapps@gmail.com>
2018-07-06 08:42:51 +0200

    Security ROLES

--- src/CommonBundle/Voter/DefaultVoter.php
+++ src/CommonBundle/Voter/DefaultVoter.php
@@ -4,7 +4,37 @@
 namespace CommonBundle\Voter;
 
 
-class DefaultVoter
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class DefaultVoter extends Voter
 {
 
+    /**
+     * Determines if the attribute and subject are supported by this voter.
+     *
+     * @param string $attribute An attribute
+     * @param mixed $subject The subject to secure, e.g. an object the user wants to access or any other PHP type
+     *
+     * @return bool True if the attribute and subject are supported, false otherwise
+     */
+    protected function supports($attribute, $subject)
+    {
+        // TODO: Implement supports() method.
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     *
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        // TODO: Implement voteOnAttribute() method.
+    }
 }
