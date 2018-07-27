39302ea5fe30c55c148e0baacf994f24a77ea6d0
lucas.reliefapps <lucas.reliefapps@gmail.com>
2018-07-27 09:18:45 +0200

    clean

--- src/DistributionBundle/Controller/CriteriaDistributionController.php
+++ src/DistributionBundle/Controller/CriteriaDistributionController.php
@@ -75,7 +75,6 @@ class CriteriaDistributionController extends Controller
         }
         catch (\Exception $exception)
         {
-            dump($exception);
             return new Response($exception->getMessage(), 500);
         }
 
