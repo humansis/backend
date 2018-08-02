bf0e56a225df7002f8810197089b9c6be0acb197
JuliePetitjean <julie.reliefapps@gmail.com>
2018-08-02 12:18:42 +0200

    add function to sort by fraquency in distribution retrievers

--- src/ReportingBundle/Utils/DataRetrievers/ProjectDataRetrievers.php
+++ src/ReportingBundle/Utils/DataRetrievers/ProjectDataRetrievers.php
@@ -18,55 +18,6 @@ class ProjectDataRetrievers
     }
 
     /**
-     * Use to verify if a key project exist in filter
-     * If this key exists, it means a project was selected in selector
-     */
-    public function ifInProject($qb, array $filters) {
-        if(array_key_exists('project', $filters)) {
-            $qb->andWhere('p.id IN (:projects)')
-                    ->setParameter('projects', $filters['project']);
-        }
-        return $qb;
-    }
-
-    /**
-     * Use to make join and where in DQL
-     * Use in all project data retrievers
-     */
-    public function getReportingValue(string $code, array $filters) {
-        $qb = $this->reportingProject->createQueryBuilder('rp')
-                                    ->leftjoin('rp.value', 'rv')
-                                    ->leftjoin('rp.indicator', 'ri')
-                                    ->leftjoin('rp.project', 'p')
-                                    ->where('ri.code = :code')
-                                    ->setParameter('code', $code)
-                                    ->andWhere('p.iso3 = :country')
-                                    ->setParameter('country', $filters['country']);
-        $qb = $this->ifInProject($qb, $filters);
-
-        return $qb;
-    }
-
-    /**
-     * Get the data with the more recent values
-     */
-    public function lastDate(array $values) {
-        $moreRecentValues = [];
-        $lastDate = $values[0]['date'];
-        foreach($values as $value) {
-            if ($value['date'] > $lastDate) {
-                $lastDate = $value['date'];
-            }
-        }
-        foreach($values as $value) {
-            if ($value['date'] === $lastDate) {
-                array_push($moreRecentValues, $value);
-            }
-        }
-        return $moreRecentValues;
-    }
-
-    /**
      * Get the name of all donors
      */
     public function BMS_Project_D(array $filters) {
@@ -85,10 +36,14 @@ class ProjectDataRetrievers
      */
     public function BMS_Project_HS(array $filters) {
         $qb = $this->getReportingValue('BMS_Project_HS', $filters);
-        $qb->select('p.name AS name','rv.value AS value', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
         if (sizeof($qb->getQuery()->getArrayResult()) > 0) {
-            $result = $this->lastDate($qb->getQuery()->getArrayResult());
-            return $result;
+            $frequency = $this->getByFrequency($qb, $filters, 'BMS_Project_HS');
+            if(sizeof($frequency) > 0) {
+                $result = $this->lastDate($frequency);
+                return $result;
+            } else { 
+                return [];
+            }
         } else {
             return $qb->getQuery()->getArrayResult();
         }
@@ -99,11 +54,14 @@ class ProjectDataRetrievers
      */
     public function BMS_Project_AB(array $filters) {
         $qb = $this->getReportingValue('BMS_Project_AB', $filters);
-        $qb->select('SUM(rv.value) AS value', 'rv.unity AS name', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date")
-           ->groupBy('name', 'date');
         if (sizeof($qb->getQuery()->getArrayResult()) > 0) {
-            $result = $this->lastDate($qb->getQuery()->getArrayResult());
-            return $result;
+            $frequency = $this->getByFrequency($qb, $filters, 'BMS_Project_AB');
+            if(sizeof($frequency) > 0) {
+                $result = $this->lastDate($frequency);
+                return $result;
+            } else { 
+                return [];
+            }
         } else {
             return $qb->getQuery()->getArrayResult();
         }
@@ -159,8 +117,8 @@ class ProjectDataRetrievers
      */
     public function BMSU_Project_NM(array $filters) {
         $qb = $this->getReportingValue('BMSU_Project_NM', $filters);
-        $qb->select('rv.value AS value', "CONCAT(rv.unity, '/', p.name) AS name", "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
-        return $qb->getQuery()->getArrayResult();
+        $result = $this->getByFrequency($qb, $filters, 'BMSU_Project_NM' );
+        return $result;    
     }
 
     /**
@@ -168,8 +126,8 @@ class ProjectDataRetrievers
      */
     public function BMSU_Project_NW(array $filters) {
         $qb = $this->getReportingValue('BMSU_Project_NW', $filters);
-        $qb->select('rv.value AS value', "CONCAT(rv.unity, '/', p.name) AS name", "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
-        return $qb->getQuery()->getArrayResult();
+        $result = $this->getByFrequency($qb, $filters, 'BMSU_Project_NW');
+        return $result;   
     }  
     
     /**
@@ -217,9 +175,8 @@ class ProjectDataRetrievers
      */
     public function BMSU_Project_TVSV(array $filters) {
         $qb = $this->getReportingValue('BMSU_Project_TVSV', $filters);
-        $qb->select('SUM(rv.value) AS value', 'rv.unity AS unity',  "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date")
-           ->groupBy('unity', 'date');
-        return $qb->getQuery()->getArrayResult();
+        $result = $this->getByFrequency($qb, $filters, 'BMSU_Project_TVSV' );
+        return $result;
     }
 
     /**
@@ -227,9 +184,8 @@ class ProjectDataRetrievers
      */
     public function BMSU_Project_TVS(array $filters) {
         $qb = $this->getReportingValue('BMSU_Project_TVS', $filters);
-        $qb->select('SUM(rv.value) AS value', 'rv.unity AS unity',  "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date")
-           ->groupBy('unity', 'date');        
-        return $qb->getQuery()->getArrayResult();
+        $result = $this->getByFrequency($qb, $filters, 'BMSU_Project_TVS' );
+        return $result;
     }
 
     /**
@@ -237,12 +193,172 @@ class ProjectDataRetrievers
      */
     public function BMSU_Project_PV(array $filters) {
         $qb = $this->getReportingValue('BMSU_Project_PV', $filters);
-        $qb->select('rv.value AS value', 'p.name AS name', 'p.id as id',  "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");     
-        return $qb->getQuery()->getArrayResult();
+        $result = $this->getByFrequency($qb, $filters, 'BMSU_Project_PV' );
+        return $result;
+    }
+
+    /**
+     * Use to verify if a key project exist in filter
+     * If this key exists, it means a project was selected in selector
+     */
+    public function ifInProject($qb, array $filters) {
+        if(array_key_exists('project', $filters)) {
+            $qb->andWhere('p.id IN (:projects)')
+                    ->setParameter('projects', $filters['project']);
+        }
+        return $qb;
+    }
+
+    /**
+     * Use to make join and where in DQL
+     * Use in all project data retrievers
+     */
+    public function getReportingValue(string $code, array $filters) {
+        $qb = $this->reportingProject->createQueryBuilder('rp')
+                                    ->leftjoin('rp.value', 'rv')
+                                    ->leftjoin('rp.indicator', 'ri')
+                                    ->leftjoin('rp.project', 'p')
+                                    ->where('ri.code = :code')
+                                    ->setParameter('code', $code)
+                                    ->andWhere('p.iso3 = :country')
+                                    ->setParameter('country', $filters['country']);
+        $qb = $this->ifInProject($qb, $filters);
+        dump($qb->getQuery()->getArrayResult());
+
+        return $qb;
+    }
+
+    /**
+     * sort data by frequency
+     */
+    public function getByFrequency($qb, array $filters, string $nameFunction) {
+        if ($filters['frequency'] === "Month") {
+          $qb ->andWhere("MONTH(rv.creationDate) = MONTH(CURRENT_DATE())");
+          $qb = $this->conditionSelect($qb, $nameFunction, 'Month');
+          $result = $qb->getQuery()->getArrayResult();
+        }
+        else if($filters['frequency'] === "Year") {
+          $qb ->andWhere("YEAR(rv.creationDate) = YEAR(CURRENT_DATE())");
+          $qb = $this->conditionSelect($qb, $nameFunction, 'Year');
+          $result = $qb->getQuery()->getArrayResult();
+        } 
+        else if($filters['frequency'] === "Quarter") {
+          $qb ->andWhere("QUARTER(rv.creationDate) = QUARTER(CURRENT_DATE())");
+          $qb = $this->conditionSelect($qb, $nameFunction, 'Quarter');
+          $byQuarter = $qb->getQuery()->getArrayResult();
+          $result = $this->getNameQuarter($byQuarter);
+        } 
+        else {
+            $period = explode('-', $filters['frequency']); 
+            $qb ->andWhere("DATE_FORMAT(rv.creationDate, '%m/%d/%Y')  >= :from")
+                    ->setParameter('from', $period[0])
+                ->andWhere("DATE_FORMAT(rv.creationDate, '%m/%d/%Y')  <= :to")
+                    ->setParameter('to', $period[1]);
+            $qb = $this->conditionSelect($qb, $nameFunction, 'Period');
+            $result = $qb->getQuery()->getArrayResult();
+        }
+        return $result;
+    }
+
+    public function conditionSelect($qb, $nameFunction, $frequency) {
+        switch ($nameFunction) {
+            case 'BMS_Project_AB':
+                if ($frequency === 'Month' || $frequency === "Period") {
+                    $qb ->select('rv.unity AS name','SUM(rv.value) AS value', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date")
+                        ->groupBy('name', 'date');
+                } else if ($frequency === 'Year') {
+                    $qb ->select('rv.unity AS name','SUM(rv.value) AS value', "DATE_FORMAT(rv.creationDate, '%Y') AS date")
+                        ->groupBy('name', 'date');
+                } else if ($frequency === 'Quarter') {
+                    $qb ->select('rv.unity AS name','SUM(rv.value) AS value', "QUARTER(DATE_FORMAT(rv.creationDate, '%Y-%m-%d')) AS date")
+                        ->groupBy('name', 'date');
+                } 
+                return $qb;
+            case 'BMS_Project_HS' :
+                if ($frequency === 'Month' || $frequency === "Period") {
+                    $qb ->select('p.name AS name','rv.value AS value', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");    
+                } else if ($frequency === 'Year') {
+                    $qb ->select('p.name AS name','MAX(rv.value) AS value', "DATE_FORMAT(rv.creationDate, '%Y') AS date")
+                        ->groupBy('name', 'date');
+                } else if ($frequency === 'Quarter') {
+                    $qb ->select('p.name AS name','MAX(rv.value)  AS value', "QUARTER(DATE_FORMAT(rv.creationDate, '%Y-%m-%d')) AS date")
+                        ->groupBy('name', 'date');
+                }
+                return $qb;
+            case 'BMSU_Project_NM' :
+            case 'BMSU_Project_NW' :
+                 if ($frequency === 'Month' || $frequency === "Period") {
+                    $qb ->select('rv.value AS value', "CONCAT(rv.unity, '/', p.name) AS name", "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
+                } else if ($frequency === 'Year') {
+                    $qb ->select('MAX(rv.value)  AS value', "CONCAT(rv.unity, '/', p.name) AS name", "DATE_FORMAT(rv.creationDate, '%Y') AS date")
+                        ->groupBy('name', 'date');
+                } else if ($frequency === 'Quarter') {
+                    $qb ->select('MAX(rv.value)  AS value', "CONCAT(rv.unity, '/', p.name) AS name", "QUARTER(DATE_FORMAT(rv.creationDate, '%Y-%m-%d')) AS date")
+                        ->groupBy('name', 'date');
+                }
+                return $qb;
+            case 'BMSU_Project_TVS':
+            case 'BMSU_Project_TVSV' :
+                if ($frequency === 'Month' || $frequency === "Period") {
+                    $qb->select('SUM(rv.value) AS value', 'rv.unity AS unity',  "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date")
+                    ->groupBy('unity', 'date');           
+                } else if ($frequency === 'Year') {
+                    $qb->select('SUM(rv.value) AS value', 'rv.unity AS unity',  "DATE_FORMAT(rv.creationDate, '%Y') AS date")
+                    ->groupBy('unity', 'date');
+                } else if ($frequency === 'Quarter') {
+                    $qb->select('SUM(rv.value) AS value', 'rv.unity AS unity',  "QUARTER(DATE_FORMAT(rv.creationDate, '%Y-%m-%d')) AS date")
+                    ->groupBy('unity', 'date');
+                }  
+                return $qb;
+            case 'BMSU_Project_PV' :
+                if ($frequency === 'Month' || $frequency === "Period") {
+                    $qb ->select('rv.value AS value', 'p.name AS name', 'p.id as id',  "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");          
+                } else if ($frequency === 'Year') {
+                    $qb ->select('MAX(rv.value)  AS value', 'p.name AS name', 'p.id as id',  "DATE_FORMAT(rv.creationDate, '%Y') AS date")
+                        ->groupBy('name', 'date');
+                } else if ($frequency === 'Quarter') {
+                    $qb ->select('MAX(rv.value)  AS value', 'p.name AS name', 'p.id as id',  "DATE_FORMAT(rv.creationDate, '%Y') AS date")
+                        ->groupBy('name', 'date');
+                }
+                return $qb;     
+        }
+    }
+
+    /**
+     * get the name of month which delimit the quarter
+     */
+    public function getNameQuarter($results) {
+        foreach($results as &$result) {
+            if ($result['date'] === "1") {
+            $result["date"] = "Jan-Mar";
+            } else if ($result['date'] === "2") {
+            $result["date"] = "Apr-Jun";
+            } else if ($result['date'] === "3") {
+            $result["date"] = "Jul-Sep";
+            } else {
+            $result["date"] = "Oct-Dec";
+            }
+        }
+        return $results;
     }
 
 
-
-
-
+    /**
+     * Get the data with the more recent values
+     */
+    public function lastDate(array $values) {
+        $moreRecentValues = [];
+        $lastDate = $values[0]['date'];
+        foreach($values as $value) {
+            if ($value['date'] > $lastDate) {
+                $lastDate = $value['date'];
+            }
+        }
+        foreach($values as $value) {
+            if ($value['date'] === $lastDate) {
+                array_push($moreRecentValues, $value);
+            }
+        }
+        return $moreRecentValues;
+    }
 }
