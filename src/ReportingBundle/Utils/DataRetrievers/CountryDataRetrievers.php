8a335f4e00294c0094e3b1d2aa0c9fd06a38162d
JuliePetitjean <julie.reliefapps@gmail.com>
2018-08-02 10:14:46 +0200

    add function to sort by frequency in country fillers

--- src/ReportingBundle/Utils/DataRetrievers/CountryDataRetrievers.php
+++ src/ReportingBundle/Utils/DataRetrievers/CountryDataRetrievers.php
@@ -34,67 +34,116 @@ class CountryDataRetrievers
       return $qb;
   }
 
-
-    /**
-     * Get total of household by country
-     */
-    public function BMS_Country_TH(array $filters)
-    {
-      $qb = $this->getReportingValue('BMS_Country_TH', $filters);
+  public function getByFrequency($qb, array $filters) {
+    if ($filters['frequency'] === "Month") {
       $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
-      return $qb->getQuery()->getArrayResult();
+      $result = $qb->getQuery()->getArrayResult();
     }
+    else if($filters['frequency'] === "Year") {
+      $qb ->select('rc.country AS name','MAX(rv.value) AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y') AS date")
+          ->groupBy('name', 'unity', 'date');
+      $result = $qb->getQuery()->getArrayResult();
+    } 
+    else if($filters['frequency'] === "Quarter") {
+      $qb ->select('rc.country AS name','MAX(rv.value) AS value', 'rv.unity AS unity', "QUARTER(DATE_FORMAT(rv.creationDate, '%Y-%m-%d')) AS date")
+          ->groupBy('name', 'unity', 'date');
+      $byQuarter = $qb->getQuery()->getArrayResult();
+      $result = $this->getNameQuarter($byQuarter);
+    } 
+    else {
+      $period = explode('-', $filters['frequency']); 
+      $qb ->andWhere("DATE_FORMAT(rv.creationDate, '%m/%d/%Y')  >= :from")
+              ->setParameter('from', $period[0])
+          ->andWhere("DATE_FORMAT(rv.creationDate, '%m/%d/%Y')  <= :to")
+              ->setParameter('to', $period[1])
+          ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
+      $result = $qb->getQuery()->getArrayResult();
+    }
+    
+    return $result;
+  }
 
-    /**
-     * Get total of active projects by country
-     */
-    public function BMS_Country_AP(array $filters)
-    {
-      $qb = $this->getReportingValue('BMS_Country_AP', $filters);
-      $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
-      return $qb->getQuery()->getArrayResult();
+  public function getNameQuarter($results) {
+    foreach($results as &$result) {
+        if ($result['date'] === "1") {
+          $result["date"] = "Jan-Mar";
+        } else if ($result['date'] === "2") {
+          $result["date"] = "Apr-Jun";
+        } else if ($result['date'] === "3") {
+          $result["date"] = "Jul-Sep";
+        } else {
+          $result["date"] = "Oct-Dec";
+        }
     }
-
-    /**
-     * Get total funding by country
-     */
-    public function BMS_Country_TF(array $filters)
-    {
-      $qb = $this->getReportingValue('BMS_Country_TF', $filters);
-      $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
-      return $qb->getQuery()->getArrayResult();
-    }
-
-    /**
-     * Get total of enrolled beneficiaries by country
-     */
-    public function BMS_Country_EB(array $filters)
-    {
-      $qb = $this->getReportingValue('BMS_Country_EB', $filters);
-      $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
-      return $qb->getQuery()->getArrayResult();
-    }
-
-    /**
-     * Get total number of distributions by country
-     */
-    public function BMS_Country_TND(array $filters)
-    {
-      $qb = $this->getReportingValue('BMS_Country_TND', $filters);
-      $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
-      return $qb->getQuery()->getArrayResult();
-    }
+    return $results;
+  }
 
 
-    /**
-     * Get total transactions completed
-     */
-    public function BMS_Country_TTC(array $filters)
-    {
-      $qb = $this->getReportingValue('BMS_Country_TTC', $filters);
-      $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
-      return $qb->getQuery()->getArrayResult();
-    }
+  /**
+   * Get total of household by country
+   */
+  public function BMS_Country_TH(array $filters)
+  {
+    $qb = $this->getReportingValue('BMS_Country_TH', $filters);
+    $result = $this->getByFrequency($qb, $filters);
+    return $result;
+  }
+
+  /**
+   * Get total of active projects by country
+   */
+  public function BMS_Country_AP(array $filters)
+  {
+    $qb = $this->getReportingValue('BMS_Country_AP', $filters);
+    $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
+    $result = $this->getByFrequency($qb, $filters);
+    return $result;
+  }
+
+  /**
+   * Get total funding by country
+   */
+  public function BMS_Country_TF(array $filters)
+  {
+    $qb = $this->getReportingValue('BMS_Country_TF', $filters);
+    $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
+    $result = $this->getByFrequency($qb, $filters);
+    return $result;
+  }
+
+  /**
+   * Get total of enrolled beneficiaries by country
+   */
+  public function BMS_Country_EB(array $filters)
+  {
+    $qb = $this->getReportingValue('BMS_Country_EB', $filters);
+    $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
+    $result = $this->getByFrequency($qb, $filters);
+    return $result;
+  }
+
+  /**
+   * Get total number of distributions by country
+   */
+  public function BMS_Country_TND(array $filters)
+  {
+    $qb = $this->getReportingValue('BMS_Country_TND', $filters);
+    $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
+    $result = $this->getByFrequency($qb, $filters);
+    return $result;
+  }
+
+
+  /**
+   * Get total transactions completed
+   */
+  public function BMS_Country_TTC(array $filters)
+  {
+    $qb = $this->getReportingValue('BMS_Country_TTC', $filters);
+    $qb ->select('rc.country AS name','rv.value AS value', 'rv.unity AS unity', "DATE_FORMAT(rv.creationDate, '%Y-%m-%d') AS date");
+    $result = $this->getByFrequency($qb, $filters);
+    return $result;
+  }
 
 
 
