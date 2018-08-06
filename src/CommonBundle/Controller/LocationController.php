816c7cf8a0324d43a0c75c82a333a4e84e41f58d
JuliePetitjean <julie.reliefapps@gmail.com>
2018-08-06 12:32:37 +0200

    add controller location

---
+++ src/CommonBundle/Controller/LocationController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CommonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class LocationController extends Controller
+{
+}
