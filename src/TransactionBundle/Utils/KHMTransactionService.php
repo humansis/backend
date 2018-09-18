ef7e0de355380fd722d4abfb7b3810680179ab25
Barbara <barbara.reliefapps@gmail.com>
2018-09-18 08:44:20 +0200

    test transaction with wing api

--- src/TransactionBundle/Utils/KHMTransactionService.php
+++ src/TransactionBundle/Utils/KHMTransactionService.php
@@ -30,13 +30,25 @@ class KHMTransactionService extends DefaultTransactionService {
     public function getToken()
     {
         $query = array(
-            'username' => 'thirdParty',
-            'password' => '@pIw!n9$#',
-            'client_id' => 'third_party',
-            'client_secret' => '16681c9ff419d8ecc7cfe479eb02a7a'
+            'username'      => 'thirdParty',
+            'password'      => 'ba0228f6e48ba7942d79e2b44e6072ee',
+            'grant_type'    => 'password',
+            'client_id'     => 'third_party',
+            'client_secret' => '16681c9ff419d8ecc7cfe479eb02a7a',
+            'scope'         => 'trust'
         );
+        
+        // $method = 'aes256';
+        // $key = hash('sha256', 'HG58YZ3CR9');
+        // $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));
+        // dump($method, $key);
+        // $encrypted = base64_encode(openssl_encrypt('wingmoney', $method, $key, OPENSSL_RAW_DATA, $iv));
+        // dump($encrypted);
+        // $decrypted = openssl_decrypt(base64_decode('TsvWLCay+F+erP167XyzBw=='), $method, $key, OPENSSL_RAW_DATA, $iv);
+        // dump($decrypted);
+        // 
         $response = Unirest\Request::post($this->url . "/oauth/token", null, $query);
-        return $response->raw_body;
+        return $response->body;
     }
 
 }
