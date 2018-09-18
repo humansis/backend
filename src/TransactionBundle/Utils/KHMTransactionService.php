894078f5b493ec90773964606289d1fb5f8d69ce
Barbara <barbara.reliefapps@gmail.com>
2018-09-18 10:28:02 +0200

    test transaction with unirest

--- src/TransactionBundle/Utils/KHMTransactionService.php
+++ src/TransactionBundle/Utils/KHMTransactionService.php
@@ -29,57 +29,66 @@ class KHMTransactionService extends DefaultTransactionService {
      */
     public function getToken()
     {
-        // $query = array(
-        //     'username'      => 'thirdParty',
-        //     'password'      => 'ba0228f6e48ba7942d79e2b44e6072ee',
-        //     'grant_type'    => 'password',
-        //     'client_id'     => 'third_party',
-        //     'client_secret' => '16681c9ff419d8ecc7cfe479eb02a7a',
-        //     'scope'         => 'trust'
-        // );
-        // 
-        // // $method = 'aes256';
-        // // $key = hash('sha256', 'HG58YZ3CR9');
-        // // $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));
-        // // dump($method, $key);
-        // // $encrypted = base64_encode(openssl_encrypt('wingmoney', $method, $key, OPENSSL_RAW_DATA, $iv));
-        // // dump($encrypted);
-        // // $decrypted = openssl_decrypt(base64_decode('TsvWLCay+F+erP167XyzBw=='), $method, $key, OPENSSL_RAW_DATA, $iv);
-        // // dump($decrypted);
-        // // 
-        // $response = Unirest\Request::post($this->url . "/oauth/token", null, $query);
-        // dump($response);
-        // return $response->body;
-         
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-          CURLOPT_PORT => "8443",
-          CURLOPT_URL => "https://stageonline.wingmoney.com:8443/RestEngine/oauth/token",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 30,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => "username=thirdParty&password=ba0228f6e48ba7942d79e2b44e6072ee&grant_type=password&client_id=third_party&client_secret=16681c9ff419d8ecc7cfe479eb02a7a&scope=trust",
-          CURLOPT_HTTPHEADER => array(
-          ),
-        ));
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+        $query = array(
+            'username'      => 'thirdParty',
+            'password'      => 'ba0228f6e48ba7942d79e2b44e6072ee',
+            'grant_type'    => 'password',
+            'client_id'     => 'third_party',
+            'client_secret' => '16681c9ff419d8ecc7cfe479eb02a7a',
+            'scope'         => 'trust'
+        );
         
+        // $method = 'aes256';
+        // $key = hash('sha256', 'HG58YZ3CR9');
+        // $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($method));
+        // dump($method, $key);
+        // $encrypted = base64_encode(openssl_encrypt('wingmoney', $method, $key, OPENSSL_RAW_DATA, $iv));
+        // dump($encrypted);
+        // $decrypted = openssl_decrypt(base64_decode('TsvWLCay+F+erP167XyzBw=='), $method, $key, OPENSSL_RAW_DATA, $iv);
+        // dump($decrypted);
+         
+        
+        Unirest\Request::curlOpts(array(
+            CURLOPT_PORT => '8443',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        ));
+        $response = Unirest\Request::post($this->url . "/oauth/token", null, $query);
         dump($response);
-        dump($err);
-
-        curl_close($curl);
-
-        if ($err) {
-          return $err;
-        } else {
-          return $response;
-        }
+        return $response->body;
+         
+        // $curl = curl_init();
+        // 
+        // curl_setopt_array($curl, array(
+        //   CURLOPT_PORT => "8443",
+        //   CURLOPT_URL => "https://stageonline.wingmoney.com:8443/RestEngine/oauth/token",
+        //   CURLOPT_RETURNTRANSFER => true,
+        //   CURLOPT_ENCODING => "",
+        //   CURLOPT_MAXREDIRS => 10,
+        //   CURLOPT_TIMEOUT => 30,
+        //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        //   CURLOPT_CUSTOMREQUEST => "POST",
+        //   CURLOPT_POSTFIELDS => "username=thirdParty&password=ba0228f6e48ba7942d79e2b44e6072ee&grant_type=password&client_id=third_party&client_secret=16681c9ff419d8ecc7cfe479eb02a7a&scope=trust",
+        //   CURLOPT_HTTPHEADER => array(
+        //   ),
+        // ));
+        // 
+        // $response = curl_exec($curl);
+        // $err = curl_error($curl);
+        // 
+        // dump($response);
+        // dump($err);
+        // 
+        // curl_close($curl);
+        // 
+        // if ($err) {
+        //   return $err;
+        // } else {
+        //   return $response;
+        // }
     }
 
 }
