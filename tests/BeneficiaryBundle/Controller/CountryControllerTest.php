31be74057d776495eb51bdf1286106f722038457
Alexandre Moray <alexandre.reliefapps@gmail.com>
2018-11-28 11:52:43 +0100

    finished country specifics

--- tests/BeneficiaryBundle/Controller/CountryControllerTest.php
+++ tests/BeneficiaryBundle/Controller/CountryControllerTest.php
@@ -20,14 +20,26 @@ class CountryControllerTest extends BMSServiceTestCase {
         $this->client = $this->container->get('test.client');
     }
 
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function testCreateAction() {
         $user = $this->getTestUser(self::USER_TESTER);
         $token = $this->getUserToken($user);
         $this->tokenStorage->setToken($token);
 
-        $countryResponse = $this->request('PUT', 'api/wsse/country_specifics');
+        $body;
+        $body['countryIso3'] = "";
+        $body['field'] = "test1";
+        $body['name'] = "";
+        $body['type'] = "text";
+
+        $countryResponse = $this->request('PUT', 'api/wsse/country_specifics', $body);
         $listCountry = json_decode($this->client->getResponse()->getContent(), true);
 
+        dump($listCountry);
         $this->assertTrue($this->client->getResponse()->isSuccessful());
         $this->assertArrayHasKey('id', $listCountry);
         $this->assertArrayHasKey('field_string', $listCountry);
@@ -56,15 +68,21 @@ class CountryControllerTest extends BMSServiceTestCase {
 
     /**
      * @depends testCreateAction
-     * @param CountrySpecific $objectCountry
+     * @param $objectCountry
      * @return void
      */
-    public function testUpdateAction() {
+    public function testUpdateAction($objectCountry) {
         $user = $this->getTestUser(self::USER_TESTER);
         $token = $this->getUserToken($user);
         $this->tokenStorage->setToken($token);
 
-        $countryResponse = $this->request('POST', 'api/wsse/country_specifics/' . $objectCountry['id']);
+        $body;
+        $body['countryIso3'] = "";
+        $body['field'] = "test2";
+        $body['name'] = "";
+        $body['type'] = "text";
+
+        $countryResponse = $this->request('POST', 'api/wsse/country_specifics/' . $objectCountry['id'], $body);
         $listCountry = json_decode($this->client->getResponse()->getContent(), true);
 
         $this->assertTrue($this->client->getResponse()->isSuccessful());
@@ -78,10 +96,10 @@ class CountryControllerTest extends BMSServiceTestCase {
 
     /**
      * @depends testCreateAction
-     * @param CountrySpecific $objectCountry
+     * @param $objectCountry
      * @return void
      */
-    public function testDeleteAction() {
+    public function testDeleteAction($objectCountry) {
         $user = $this->getTestUser(self::USER_TESTER);
         $token = $this->getUserToken($user);
         $this->tokenStorage->setToken($token);
