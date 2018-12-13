ac05cf36855b6235357e5e1d1246be38b523a74a
Alexandre Moray <alexandre.reliefapps@gmail.com>
2018-12-13 10:28:02 +0100

    change template format for households

--- src/CommonBundle/Controller/ExportController.php
+++ src/CommonBundle/Controller/ExportController.php
@@ -76,7 +76,9 @@ class ExportController extends Controller
             }
             elseif ($request->query->get('householdsTemplate')) {
                 $countryIso3 = $request->request->get("__country");
+                dump('entered');
                 $filename = $this->get('beneficiary.household_export_csv_service')->exportToCsv($type, $countryIso3);
+                dump('got');
             }
 
             // Create binary file to send
