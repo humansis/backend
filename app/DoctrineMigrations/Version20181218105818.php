9c59960ad6423f9279076228bc79a43b9bc1177b
Mathias <mathias.reliefapps@gmail.com>
2018-12-18 11:58:46 +0100

    new migration

--- app/DoctrineMigrations/Version20181218105818.php
+++ app/DoctrineMigrations/Version20181218105818.php
@@ -8,14 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181217111634 extends AbstractMigration
+final class Version20181218105818 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE wing (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, salt VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_B8020544F85E0677 (username), UNIQUE INDEX UNIQ_B80205448FFBE0F7 (salt), UNIQUE INDEX UNIQ_B802054435C246D5 (password), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE financial_provider (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_3FF138985373C966 (country), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -23,6 +23,6 @@ final class Version20181217111634 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE wing');
+        $this->addSql('DROP TABLE financial_provider');
     }
 }
