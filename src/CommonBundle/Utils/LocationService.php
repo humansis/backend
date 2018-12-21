1212f673217f7a6b3b36fb6a4365371eb4b16822
Mathias <mathias.reliefapps@gmail.com>
2018-12-21 11:15:37 +0100

    Fix issue with location

--- src/CommonBundle/Utils/LocationService.php
+++ src/CommonBundle/Utils/LocationService.php
@@ -56,54 +56,23 @@ class LocationService
             'any'
         );
 
-        $adm1 = $this->em->getRepository(Adm1::class)->findOneBy([
-            "countryISO3" => $countryISO3,
-            "name" => $locationArray["adm1"]
-        ]);
+        // Define location array
+        $adm1 = $this->em->getRepository(Adm1::class)->findOneBy(["name" => $locationArray['adm1']]);
+        $adm2 = $this->em->getRepository(Adm2::class)->findOneBy(["name" => $locationArray['adm2']]);
+        $adm3 = $this->em->getRepository(Adm3::class)->findOneBy(["name" => $locationArray['adm3']]);
+        $adm4 = $this->em->getRepository(Adm4::class)->findOneBy(["name" => $locationArray['adm4']]);
 
-        if ($adm1 instanceof Adm1)
-        {
-            if (!array_key_exists("adm2", $locationArray) || null === $locationArray["adm2"] || "" === $locationArray["adm2"])
-                return $adm1->getLocation();
-            $adm2 = $this->em->getRepository(Adm2::class)->findOneBy([
-                "adm1" => $adm1,
-                "name" => $locationArray["adm2"]
-            ]);
-
-            if ($adm2 instanceof Adm2)
-            {
-                if (!array_key_exists("adm3", $locationArray) || null === $locationArray["adm3"] || "" === $locationArray["adm3"])
-                    return $adm2->getLocation();
-                $adm3 = $this->em->getRepository(Adm3::class)->findOneBy([
-                    "adm2" => $adm2,
-                    "name" => $locationArray["adm3"]
-                ]);
-
-                if ($adm3 instanceof Adm3)
-                {
-                    if (!array_key_exists("adm4", $locationArray) || null === $locationArray["adm4"] || "" === $locationArray["adm4"])
-                        return $adm3->getLocation();
-                    $adm4 = $this->em->getRepository(Adm4::class)->findOneBy([
-                        "adm3" => $adm3,
-                        "name" => $locationArray["adm4"]
-                    ]);
-
-                    if ($adm4 instanceof Adm4)
-                    {
-                        return $adm4->getLocation();
-                    }
-
-                    return $adm3->getLocation();
-                }
-                else
-                {
-                    return $adm2->getLocation();
-                }
-            }
-            else
-            {
-                return $adm1->getLocation();
-            }
+        if ($adm4 instanceof Adm4) {
+            return $adm4->getLocation();
+        }
+        if ($adm3 instanceof Adm3) {
+            return $adm3->getLocation();
+        }
+        if ($adm2 instanceof Adm2) {
+            return $adm2->getLocation();
+        }
+        if ($adm1 instanceof Adm1) {
+            return $adm1->getLocation();
         }
 
         return null;
