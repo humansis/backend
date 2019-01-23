514912cf315125aac8fab98acb47c36e0e6443d8
Francis Wardley <fwardley90@gmail.com>
2019-01-23 09:35:06 +0100

    initial changes to Booklet CRUD

--- src/VoucherBundle/Utils/BookletService.php
+++ src/VoucherBundle/Utils/BookletService.php
@@ -45,19 +45,7 @@ class BookletService
    */
   public function create(Booklet $booklet, array $bookletData)
   {
-    $allBooklets = $this->em->getRepository(Booklet::class)->findAll();
-    $bookletBatch;
-    end($allBooklets);
 
-    if (count($allBooklets) > 1) {
-      $bookletBatch = sprintf("%03d", $allBooklets[key($allBooklets)]->getId() + 1);
-    } elseif (count($allBooklets) == 1) {
-      $bookletBatch = "002";
-    } elseif (!allBooklets) {
-      $bookletBatch = "001";
-    }
-    var_dump(gettype($bookletBatch));
-    // var_dump($allBooklets);
 
     for ($x = 0; $x <= $bookletData['numberBooklets']; $x++) {
       // $booklet->setCode($vendorData['name']);
@@ -75,79 +63,28 @@ class BookletService
     return $booklet;
   }
 
+  /**
+   * @param Booklet $booklet
+   * @param array $bookletData
+   * @return Booklet
+   */
+  public function generateCode(Booklet $booklet, array $bookletData)
+  {
+    // // CREATE BOOKLET CODE #1stBatchNumber-lastBatchNumber-BookletId
+    // $allBooklets = $this->em->getRepository(Booklet::class)->findAll();
+    // $bookletBatch;
+    // end($allBooklets);
 
-  // public function findAll()
-  // {
-  //   return $this->em->getRepository(Vendor::class)->findAll();
-  // }
+    // if (count($allBooklets) > 1) {
+    //   $bookletBatch = sprintf("%03d", $allBooklets[key($allBooklets)]->getId() + 1);
+    // } elseif (count($allBooklets) == 1) {
+    //   $bookletBatch = "002";
+    // } elseif (!allBooklets) {
+    //   $bookletBatch = "001";
+    // }
+    // var_dump($allBooklets);
 
-
-  // /**
-  //  * @param Vendor $vendor
-  //  * @param array $vendorData
-  //  * @return Vendor
-  //  */
-  // public function update(Vendor $vendor, array $vendorData)
-  // {
-
-  //   foreach ($vendorData as $key => $value) {
-  //     if ($key == 'name') {
-  //       $vendor->setName($value);
-  //     } elseif ($key == 'shop') {
-  //       $vendor->setShop($value);
-  //     } elseif ($key == 'address') {
-  //       $vendor->setAddress($value);
-  //     } elseif ($key == 'username') {
-  //       $vendor->setUsername($value);
-  //     } elseif ($key == 'password') {
-  //       $vendor->setPassword($value);
-  //     }
-  //   }
-
-  //   $this->em->merge($vendor);
-
-  //   $this->em->flush();
-
-  //   return $vendor;
-  // }
-
-  // /**
-  //  * Archive Vendor
-  //  *
-  //  * @param Vendor $vendor
-  //  * @param bool $archiveVendor
-  //  * @return bool
-  //  */
-  // public function archiveVendor(Vendor $vendor, bool $archiveVendor = true)
-  // {
-  //     try {
-  //       $vendor->setArchived($archiveVendor);
-  //       $this->em->merge($vendor);
-  //       $this->em->flush();
-  //     } catch (\Exception $exception) {
-  //       return $exception;
-  //     }
-  //   return $vendor;
-  // }
-
-  // /**
-  //  * Perminantly delete the record from the database
-  //  *
-  //  * @param Vendor $vendor
-  //  * @param bool $removeVendor
-  //  * @return bool
-  //  */
-  // public function deleteFromDatabase(Vendor $vendor, bool $removeVendor = true)
-  // {
-  //   if ($removeVendor) {
-  //     try {
-  //       $this->em->remove($vendor);
-  //       $this->em->flush();
-  //     } catch (\Exception $exception) {
-  //       return $exception;
-  //     }
-  //   }
-  //   return true;
-  // }
+    return $booklet;
+  }
 
 }
