dc58d765b5a96cad40a58493e54c7f90e53a01bc
Barbara <36275616+barbaraReliefapps@users.noreply.github.com>
2019-03-02 13:54:53 +0700

    Dev travis (#202)
    * test travis
    
    * test travis deploy to test server
    
    * fix travis.yml file
    
    * add permissions for deploy script
    
    * test deploy
    
    * fix deploy script
    
    * change encryption
    
    * deploy file
    
    * test
    
    * fix travis file
    
    * fix pem file permissions
    
    * fix deploy script
    
    * fix deployment via travis
    
    * fix yml parsing

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $1 == "prod" ]]; then
+    ec2="ec2-35-158-182-63.eu-central-1.compute.amazonaws.com"
+elif [[ $1 == "staging" ]]; then
+    ec2="ec2-52-57-90-156.eu-central-1.compute.amazonaws.com"
+else
+    echo "Unknown environment"
+    exit
+fi
+
+if [ -z `ssh-keygen -F $ec2` ]; then
+  ssh-keyscan -H $ec2 >> ~/.ssh/known_hosts
+fi
+
+ssh -i $2 ubuntu@$ec2 \
+    "cd /var/www/html/bms_api; \
+    git checkout $1; \
+    git pull origin $1; \
+    sudo docker-compose exec -T php bash -c 'php bin/console c:c'; \
+    sudo docker-compose exec  -T php bash -c 'php bin/console d:m:m -n'"
