3912d47f17ad7f4203d41313efb6c9abd27470e4
Barbara <barbara.reliefapps@gmail.com>
2019-03-21 12:46:18 +0100

    commit clean script

--- docker/php/clean.sh
+++ docker/php/clean.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+echo -e "** Dropping database **"
 php bin/console d:d:d --force
 rm -rf var/cache/*
+echo -e "** Creating database **"
 php bin/console d:d:c
 php bin/console d:m:m -n
 php bin/console d:f:l -n
@@ -10,6 +12,8 @@ php bin/console jrk:levenshtein:install
 php bin/console reporting:code-indicator:add
 rm -rf var/cache/*
 php bin/console cache:clear
+
+echo -e "** Starting cron **"
 status=$(printf 'symfony\n' | sudo -S service cron status)
 echo $status
 if [[ $status != 'cron is running.' ]]; then
