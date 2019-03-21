e710224e98eb83d764690d26ac5e48caf1a0261f
Barbara <barbara.reliefapps@gmail.com>
2019-03-21 12:32:09 +0100

    edit clean script

--- docker/php/clean.sh
+++ docker/php/clean.sh
@@ -10,4 +10,9 @@ php bin/console jrk:levenshtein:install
 php bin/console reporting:code-indicator:add
 rm -rf var/cache/*
 php bin/console cache:clear
-printf 'symfony\n' | sudo -S cron
+status=$(printf 'symfony\n' | sudo -S service cron status)
+echo $status
+if [[ $status != 'cron is running.' ]]; then
+    echo 'not running'
+    sudo cron
+fi
