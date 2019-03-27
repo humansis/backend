4a8880c9972c43b9e9de608f95e219b773f96360
Stanislas Bruhiere <stanbill91@gmail.com>
2019-03-27 13:29:21 +0100

    Tweak donors

--- src/ProjectBundle/Utils/DonorService.php
+++ src/ProjectBundle/Utils/DonorService.php
@@ -63,7 +63,12 @@ class DonorService
      */
     public function create(array $donorArray)
     {
-        $donor = $this->serializer->deserialize(json_encode($donorArray), Donor::class, 'json');
+        $donor = new Donor();
+
+        $donor->setFullname($donorArray["fullname"])
+            ->setShortname($donorArray["shortname"])
+            ->setNotes($donorArray["notes"]);
+
         $donor->setDateAdded(new \DateTime());
 
         $errors = $this->validator->validate($donor);
@@ -92,11 +97,12 @@ class DonorService
     public function edit(Donor $donor, array $donorArray)
     {
         /** @var Donor $editedDonor */
-        $editedDonor = $this->serializer->deserialize(json_encode($donorArray), Donor::class, 'json');
+    $donor->setFullname($donorArray["fullname"])
+        ->setShortname($donorArray["shortname"])
+        ->setNotes($donorArray["notes"]);
 
-        $editedDonor->setId($donor->getId());
 
-        $errors = $this->validator->validate($editedDonor);
+        $errors = $this->validator->validate($donor);
         if (count($errors) > 0)
         {
             $errorsArray = [];
@@ -107,10 +113,10 @@ class DonorService
             throw new \Exception(json_encode($errorsArray), Response::HTTP_BAD_REQUEST);
         }
 
-        $this->em->merge($editedDonor);
+        $this->em->merge($donor);
         $this->em->flush();
 
-        return $editedDonor;
+        return $donor;
     }
 
     /**
