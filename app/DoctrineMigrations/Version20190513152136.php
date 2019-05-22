5dd91ac62010c35b23d715078eb1f41b20af3e1f
developer3 <coline.therial@orange.fr>
2019-05-22 14:57:22 +0200

    Delete duplicated donor migration

--- app/DoctrineMigrations/Version20190513152136.php
+++ app/DoctrineMigrations/Version20190513152136.php
@@ -16,7 +16,6 @@ final class Version20190513152136 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user ADD mustChangePassword TINYINT(1) DEFAULT \'0\' NOT NULL');
-        $this->addSql('ALTER TABLE donor CHANGE notes notes VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -24,7 +23,6 @@ final class Version20190513152136 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE donor CHANGE notes notes VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
         $this->addSql('ALTER TABLE `user` DROP mustChangePassword');
     }
 }
