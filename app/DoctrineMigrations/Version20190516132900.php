3d6ba10707acf1fdb65b5d8e35a82df92e25ad76
developer3 <coline.therial@orange.fr>
2019-05-22 15:02:37 +0200

    Remove duplicated donor migration

--- app/DoctrineMigrations/Version20190516132900.php
+++ app/DoctrineMigrations/Version20190516132900.php
@@ -15,7 +15,6 @@ final class Version20190516132900 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE donor CHANGE notes notes VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE distribution_beneficiary ADD justification VARCHAR(511) DEFAULT NULL, ADD removed TINYINT(1) DEFAULT \'0\' NOT NULL');
     }
 
@@ -25,6 +24,5 @@ final class Version20190516132900 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE distribution_beneficiary DROP justification, DROP removed');
-        $this->addSql('ALTER TABLE donor CHANGE notes notes VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci');
     }
 }
