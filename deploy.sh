74e66d9446b0d7c7612605e6534e028ce8154fb6
Barbara <barbara.reliefapps@gmail.com>
2019-07-23 09:40:10 +0200

    add docker-compose for demo environment and fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,20 @@ set -e
 if [[ $1 == "master" ]]; then
     ec2_prod="ec2-35-158-182-63.eu-central-1.compute.amazonaws.com"
     ec2_demo="ec2-18-185-233-146.eu-central-1.compute.amazonaws.com"
+    if [ -z `ssh-keygen -F $ec2_prod` ]; then
+        ssh-keyscan -H $ec2_prod >> ~/.ssh/known_hosts
+    fi
+    if [ -z `ssh-keygen -F $ec2_demo` ]; then
+        ssh-keyscan -H $ec2_demo >> ~/.ssh/known_hosts
+    fi
 elif [[ $1 == "dev" ]]; then
     ec2_test="ec2-52-57-90-156.eu-central-1.compute.amazonaws.com"
+    if [ -z `ssh-keygen -F $ec2_test` ]; then
+        ssh-keyscan -H $ec2_test >> ~/.ssh/known_hosts
+    fi
 else
     echo "Unknown environment"
     exit
-fi
-
-if [ -z `ssh-keygen -F $ec2` ]; then
-  ssh-keyscan -H $ec2 >> ~/.ssh/known_hosts
 fi
 
 command="cd /var/www/html/bms_api; \
