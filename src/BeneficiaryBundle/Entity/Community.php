b9c33a089106549ccbabcfc79ecc0a5f73b0fa9e
Vit Troi Fux <vit.fux@quanti.cz>
2020-05-06 11:12:10 +0200

    #55 PIN-56: added community name

--- src/BeneficiaryBundle/Entity/Community.php
+++ src/BeneficiaryBundle/Entity/Community.php
@@ -34,6 +34,14 @@ class Community
     /**
      * @var string
      *
+     * @ORM\Column(name="contact_family_name", type="string", length=255, nullable=true)
+     * @Groups({"FullBeneficiary", "FullCommunity"})
+     */
+    private $contactFamilyName;
+
+    /**
+     * @var string
+     *
      * @ORM\Column(name="phone_number", type="string", length=45, nullable=true)
      * @Groups({"FullBeneficiary", "FullHousehold", "FullReceivers", "ValidatedDistribution"})
      */
@@ -116,6 +124,22 @@ class Community
     public function setContactName(?string $contactName): void
     {
         $this->contactName = $contactName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContactFamilyName(): string
+    {
+        return $this->contactFamilyName;
+    }
+
+    /**
+     * @param string $contactFamilyName
+     */
+    public function setContactFamilyName(string $contactFamilyName): void
+    {
+        $this->contactFamilyName = $contactFamilyName;
     }
 
     /**
