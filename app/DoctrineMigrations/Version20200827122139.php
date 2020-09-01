bc60d90ede30f5ae5b922b07d247c407b7889ae6
iarro <56431174+iarro@users.noreply.github.com>
2020-09-01 12:07:10 +0200

    PIN-429: Changed criteria structure into grouped list of criteria

---
+++ app/DoctrineMigrations/Version20200827122139.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200827122139 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE selection_criteria ADD group_number INT DEFAULT 1');
+        $this->addSql('ALTER TABLE selection_criteria CHANGE group_number group_number INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE selection_criteria DROP group_number');
+    }
+}
