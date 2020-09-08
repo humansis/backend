36ba0841e93f0a19c6749dfe5ca0aeb52aa76989
iarro <56431174+iarro@users.noreply.github.com>
2020-09-08 14:02:29 +0200

    Added search index for better performance in imports

---
+++ app/DoctrineMigrations/Version20200908120104.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200908120104 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE INDEX idx_local_name ON person (localGivenName, localFamilyName)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX idx_local_name ON person');
+    }
+}
