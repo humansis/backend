1a638874652628897db087a8109365b5f87fa2fc
iarro <56431174+iarro@users.noreply.github.com>
2020-10-08 11:06:23 +0200

    Revert "PIN-429: Simplify migration"
    This reverts commit e0ae8740a7d8cc9966673063205457cd3acf1d43.

--- app/DoctrineMigrations/Version20200827122139.php
+++ app/DoctrineMigrations/Version20200827122139.php
@@ -13,7 +13,8 @@ final class Version20200827122139 extends AbstractMigration
     {
         $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE selection_criteria ADD group_number INT NOT NULL DEFAULT 1');
+        $this->addSql('ALTER TABLE selection_criteria ADD group_number INT DEFAULT 1');
+        $this->addSql('ALTER TABLE selection_criteria CHANGE group_number group_number INT NOT NULL');
     }
 
     public function down(Schema $schema): void
