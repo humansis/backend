3cccb85f41ed2179d70db296967eccb3b9624bff
iarro <56431174+iarro@users.noreply.github.com>
2020-10-20 08:20:36 +0200

    PIN-472: Added endpoint for chosen country

--- src/CommonBundle/Controller/CountryController.php
+++ src/CommonBundle/Controller/CountryController.php
@@ -5,6 +5,7 @@ namespace CommonBundle\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Swagger\Annotations as SWG;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CountryController extends Controller
@@ -25,6 +26,29 @@ class CountryController extends Controller
             'currency' => 'UAH',
         ],
     ];
+
+    /**
+     * @Rest\Get("/countries/{iso3}")
+     *
+     * @SWG\Tag(name="Location")
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="All Available countries"
+     * )
+     *
+     * @return Response
+     */
+    public function country(string $iso3)
+    {
+        foreach (self::COUNTRIES as $country) {
+            if ($iso3 === $country['iso3']) {
+                return $this->json($country);
+            }
+        }
+
+        throw $this->createNotFoundException();
+    }
 
     /**
      * @Rest\Get("/countries")
--- tests/CommonBundle/Controller/CountryControllerTest.php
+++ tests/CommonBundle/Controller/CountryControllerTest.php
@@ -22,7 +22,29 @@ class CountryControllerTest extends BMSServiceTestCase
     /**
      * @throws \Exception
      */
-    public function testGetSummary()
+    public function testCambodiaExists()
+    {
+        // Log a user in order to go through the security firewall
+        $user = $this->getTestUser(self::USER_TESTER);
+        $token = $this->getUserToken($user);
+        $this->tokenStorage->setToken($token);
+
+        $this->request('GET', '/api/wsse/countries/KHM');
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Request failed: '.$this->client->getResponse()->getContent());
+
+        $data = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertIsArray($data);
+        $this->assertArrayHasKey('name', $data);
+        $this->assertArrayHasKey('iso3', $data);
+        $this->assertArrayHasKey('currency', $data);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testList()
     {
         // Log a user in order to go through the security firewall
         $user = $this->getTestUser(self::USER_TESTER);
