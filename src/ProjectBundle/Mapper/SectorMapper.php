44cd141c116507ce1bf7543a7ba0640b39c2917f
Vit Troi Fux <vit.fux@quanti.cz>
2020-10-30 14:05:51 +0100

    PIN-726: synchronize /sector with PUT /distribution: subSector -> subsector

--- src/ProjectBundle/Mapper/SectorMapper.php
+++ src/ProjectBundle/Mapper/SectorMapper.php
@@ -55,7 +55,7 @@ class SectorMapper
         return [
             'id' => $sector,
             'name' => $this->getLabel($sector),
-            'subSectors' => $subSectorMapped,
+            'subsectors' => $subSectorMapped,
         ];
     }
 
