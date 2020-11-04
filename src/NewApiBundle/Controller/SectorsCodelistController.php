94b6acf5ad243e580ef4cb08e63df01f9b33cf9b
Pavlix <pavel.kraus01@gmail.com>
2020-11-04 09:41:33 +0100

    PIN-712: Subsectors

--- src/NewApiBundle/Controller/SectorsCodelistController.php
+++ src/NewApiBundle/Controller/SectorsCodelistController.php
@@ -6,12 +6,22 @@ namespace NewApiBundle\Controller;
 
 use CommonBundle\Pagination\Paginator;
 use ProjectBundle\DBAL\SectorEnum;
+use ProjectBundle\DTO\Sector;
+use ProjectBundle\Utils\SectorService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use FOS\RestBundle\Controller\Annotations as Rest;
 
 class SectorsCodelistController extends Controller
 {
+    /** @var SectorService */
+    private $sectorService;
+
+    public function __construct(SectorService $sectorService)
+    {
+        $this->sectorService = $sectorService;
+    }
+
     /**
      * @Rest\Get("/sectors")
      *
@@ -22,6 +32,38 @@ class SectorsCodelistController extends Controller
         $data = self::map(SectorEnum::all());
 
         return $this->json(new Paginator($data));
+    }
+
+
+    /**
+     * @Rest\Get("/sectors/{code}/subsectors")
+     *
+     * @param int $code
+     *
+     * @return JsonResponse
+     */
+    public function getSubsectors(int $code): JsonResponse
+    {
+        $sector = SectorEnum::all()[$code];
+
+        $subsectors = $this->sectorService->getSubsBySector()[$sector];
+
+        return $this->json(self::mapSubsectors($subsectors));
+    }
+
+    private static function mapSubsectors(iterable $subsectors)
+    {
+        $data = [];
+
+        /**
+         * @var int $key
+         * @var Sector $subsector
+         */
+        foreach ($subsectors as $key => $subsector) {
+            $data[] = ['code' => $key, 'value' => $subsector->getSubSectorName()];
+        }
+
+        return $data;
     }
 
     private static function map(iterable $list): array
