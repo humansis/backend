e2368171ab555903dc6b115a2a872f8c8f7893b9
Pavlix <pavel.kraus01@gmail.com>
2020-11-04 22:43:09 +0100

    PIN-712: Assistance targets

---
+++ src/NewApiBundle/Controller/AssistanceCodelistController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace NewApiBundle\Controller;
+
+use CommonBundle\Pagination\Paginator;
+use DistributionBundle\Entity\Assistance;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AssistanceCodelistController extends Controller
+{
+    /**
+     * @Rest\Get("/assistances/targets")
+     *
+     * @return JsonResponse
+     */
+    public function getTargets(): JsonResponse
+    {
+        $data = self::map(Assistance::TYPE_TO_STRING_MAPPING);
+
+        return $this->json(new Paginator($data));
+    }
+
+    private static function map(iterable $list): array
+    {
+        $data = [];
+        foreach ($list as $key => $value) {
+            $data[] = ['code' => $key, 'value' => $value];
+        }
+
+        return $data;
+    }
+}
---
+++ tests/NewApiBundle/Controller/AssistanceCodelistControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Tests\NewApiBundle\Controller;
+
+
+use DistributionBundle\Entity\Assistance;
+use Exception;
+use ProjectBundle\Enum\Livelihood;
+use Tests\BMSServiceTestCase;
+
+class AssistanceCodelistControllerTest extends BMSServiceTestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function setUp()
+    {
+        // Configuration of BMSServiceTest
+        $this->setDefaultSerializerName('serializer');
+        parent::setUpFunctionnal();
+
+        // Get a Client instance for simulate a browser
+        $this->client = $this->container->get('test.client');
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetTargets()
+    {
+        // Log a user in order to go through the security firewall
+        $user = $this->getTestUser(self::USER_TESTER);
+        $token = $this->getUserToken($user);
+        $this->tokenStorage->setToken($token);
+
+        $this->request('GET', '/api/basic/assistances/targets');
+
+        $result = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Request failed: '.$this->client->getResponse()->getContent());
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('totalCount', $result);
+        $this->assertArrayHasKey('data', $result);
+        $this->assertIsArray($result['data']);
+        $this->assertEquals(count(Assistance::TYPE_TO_STRING_MAPPING), $result['totalCount']);
+    }
+}
