a7b5de37d92b5621bc9e0458246395686b72a043
iarro <56431174+iarro@users.noreply.github.com>
2020-11-04 12:49:37 +0100

    Fixed DB

---
+++ app/DoctrineMigrations/Version20201104114548.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20201104114548 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE household
+        CHANGE livelihood livelihood ENUM(
+            \'daily_labour\',
+            \'farming_agriculture\',
+            \'farming_livestock\',
+            \'government\',
+            \'home_duties\',
+            \'trading\',
+            \'own_business\',
+            \'textiles\'
+        ) DEFAULT NULL COMMENT \'(DC2Type:enum_livelihood)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+    }
+}
