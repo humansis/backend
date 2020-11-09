640cf6cb9e02970094249809799954be078ecd0c
Pavlix <pavel.kraus01@gmail.com>
2020-11-09 12:48:42 +0100

    PIN-712: SubSectors test

--- tests/NewApiBundle/Controller/SectorsCodelistControllerTest.php
+++ tests/NewApiBundle/Controller/SectorsCodelistControllerTest.php
@@ -26,7 +26,7 @@ class SectorsCodelistControllerTest extends BMSServiceTestCase
     /**
      * @throws Exception
      */
-    public function testGetLivelihoods()
+    public function testGetSectors()
     {
         // Log a user in order to go through the security firewall
         $user = $this->getTestUser(self::USER_TESTER);
@@ -43,5 +43,28 @@ class SectorsCodelistControllerTest extends BMSServiceTestCase
         $this->assertArrayHasKey('data', $result);
         $this->assertIsArray($result['data']);
         $this->assertEquals(count(SectorEnum::all()), $result['totalCount']);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetSubSectors()
+    {
+        $testSector = SectorEnum::all()[0];
+
+        // Log a user in order to go through the security firewall
+        $user = $this->getTestUser(self::USER_TESTER);
+        $token = $this->getUserToken($user);
+        $this->tokenStorage->setToken($token);
+
+        $this->request('GET', '/api/basic/sectors/' . $testSector . '/subsectors');
+
+        $result = json_decode($this->client->getResponse()->getContent(), true);
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Request failed: '.$this->client->getResponse()->getContent());
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('totalCount', $result);
+        $this->assertArrayHasKey('data', $result);
+        $this->assertIsArray($result['data']);
     }
 }
