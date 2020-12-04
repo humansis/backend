88f842c20a5275c4de514d12f4d1db287d3dc487
Vit Troi Fux <vit.fux@quanti.cz>
2020-12-04 14:48:59 +0100

    PIN-962: added name into Community

--- app/DoctrineMigrations/Version20201204132532.php
+++ app/DoctrineMigrations/Version20201204132532.php
@@ -8,15 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200828141752 extends AbstractMigration
+final class Version20201204132532 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE distribution_data ADD assistance_type ENUM(\'activity\', \'distribution\') NOT NULL DEFAULT \'distribution\' COMMENT \'(DC2Type:enum_assistance_type)\'');
-        $this->addSql('ALTER TABLE distribution_data CHANGE type_distribution target_type INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE community ADD name VARCHAR(100) NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -24,7 +23,6 @@ final class Version20200828141752 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE distribution_data DROP assistance_type');
-        $this->addSql('ALTER TABLE distribution_data CHANGE target_type type_distribution INT NOT NULL');
+        $this->addSql('ALTER TABLE community DROP name');
     }
 }
