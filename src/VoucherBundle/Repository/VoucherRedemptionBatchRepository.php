6ec736fd57c473cd1d8369f3ded817133641eb90
Pavlix <pavel.kraus01@gmail.com>
2020-12-07 13:27:43 +0100

    PIN-842: Voucher redemption refactor

---
+++ src/VoucherBundle/Repository/VoucherRedemptionBatchRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace VoucherBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use VoucherBundle\Entity\Vendor;
+
+class VoucherRedemptionBatchRepository extends EntityRepository
+{
+    public function getAllByVendor(Vendor $vendor)
+    {
+        return $this->createQueryBuilder('vrb')
+            ->where('vrb.vendor = :vendor')
+            ->setParameter('vendor', $vendor)
+            ->getQuery()->getResult();
+    }
+}
