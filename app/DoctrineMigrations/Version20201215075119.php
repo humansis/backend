08de530a9837cf37aeca26e543d1e088564b73f0
iarro <56431174+iarro@users.noreply.github.com>
2020-12-15 09:02:50 +0100

    PIN-1108: Added internal modality types

---
+++ app/DoctrineMigrations/Version20201215075119.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20201215075119 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE modality_type ADD internal TINYINT(1) DEFAULT \'0\' NOT NULL');
+        $this->addSql('UPDATE modality_type SET internal=1 WHERE name="Activity item"');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE modality_type DROP internal');
+    }
+}
