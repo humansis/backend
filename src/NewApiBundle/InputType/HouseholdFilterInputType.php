321e57624e1026faf478f7bc80ac295ea3906981
iarro <56431174+iarro@users.noreply.github.com>
2021-01-09 15:34:47 +0100

    PIN-1130: Added Household endpoints for new FE

---
+++ src/NewApiBundle/InputType/HouseholdFilterInputType.php
@@ -0,0 +1,193 @@
+<?php
+declare(strict_types=1);
+
+namespace NewApiBundle\InputType;
+
+use BeneficiaryBundle\Entity\Referral;
+use NewApiBundle\Request\FilterInputType\AbstractFilterInputType;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class HouseholdFilterInputType extends AbstractFilterInputType
+{
+    /**
+     * @Assert\Type("string")
+     */
+    protected $fulltext;
+
+    /**
+     * @Assert\Choice({"M", "F"})
+     */
+    protected $gender;
+
+    /**
+     * @Assert\Type("array")
+     * @Assert\All(
+     *     constraints={
+     *         @Assert\Type("integer", groups={"Strict"})
+     *     },
+     *     groups={"Strict"}
+     * )
+     */
+    protected $projects;
+
+    /**
+     * @Assert\Type("array")
+     * @Assert\All(
+     *     constraints={
+     *         @Assert\Type("integer", groups={"Strict"})
+     *     },
+     *     groups={"Strict"}
+     * )
+     */
+    protected $vulnerabilities;
+
+    /**
+     * @Assert\Type("array")
+     * @Assert\All(
+     *     constraints={
+     *         @Assert\Type("integer", groups={"Strict"})
+     *     },
+     *     groups={"Strict"}
+     * )
+     */
+    protected $nationalIds;
+
+    /**
+     * @Assert\Type("array")
+     * @Assert\All(
+     *     constraints={
+     *         @Assert\Choice(callback={"BeneficiaryBundle\Enum\ResidencyStatus", "all"})
+     *     },
+     *     groups={"Strict"}
+     * )
+     */
+    protected $residencyStatuses;
+
+    /**
+     * @Assert\Type("array")
+     * @Assert\All(
+     *     constraints={
+     *         @Assert\Choice(callback="referralTypes")
+     *     },
+     *     groups={"Strict"}
+     * )
+     */
+    protected $referralTypes;
+
+    /**
+     * @Assert\Type("array")
+     * @Assert\All(
+     *     constraints={
+     *        @Assert\Choice(callback={"ProjectBundle\Enum\Livelihood", "values"}, strict=true)
+     *     },
+     *     groups={"Strict"}
+     * )
+     */
+    protected $livelihoods;
+
+    /**
+     * @Assert\Type("array")
+     * @Assert\All(
+     *     constraints={
+     *         @Assert\Type("integer", groups={"Strict"})
+     *     },
+     *     groups={"Strict"}
+     * )
+     */
+    protected $locations;
+
+    final public static function referralTypes()
+    {
+        return array_keys(Referral::REFERRALTYPES);
+    }
+
+    public function hasFulltext(): bool
+    {
+        return $this->has('fulltext');
+    }
+
+    public function getFulltext()
+    {
+        return $this->fulltext;
+    }
+
+    public function hasGender(): bool
+    {
+        return $this->has('gender');
+    }
+
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    public function hasProjects(): bool
+    {
+        return $this->has('projects');
+    }
+
+    public function getProjects()
+    {
+        return $this->projects;
+    }
+
+    public function hasVulnerabilities(): bool
+    {
+        return $this->has('vulnerabilities');
+    }
+
+    public function getVulnerabilities()
+    {
+        return $this->vulnerabilities;
+    }
+
+    public function hasNationalIds(): bool
+    {
+        return $this->has('nationalIds');
+    }
+
+    public function getNationalIds()
+    {
+        return $this->nationalIds;
+    }
+
+    public function hasResidencyStatuses(): bool
+    {
+        return $this->has('residencyStatuses');
+    }
+
+    public function getResidencyStatuses()
+    {
+        return $this->residencyStatuses;
+    }
+
+    public function hasReferralTypes(): bool
+    {
+        return $this->has('referralTypes');
+    }
+
+    public function getReferralTypes()
+    {
+        return $this->referralTypes;
+    }
+
+    public function hasLivelihoods(): bool
+    {
+        return $this->has('livelihoods');
+    }
+
+    public function getLivelihoods()
+    {
+        return $this->livelihoods;
+    }
+
+    public function hasLocations(): bool
+    {
+        return $this->has('locations');
+    }
+
+    public function getLocations()
+    {
+        return $this->locations;
+    }
+}
