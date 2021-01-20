2d666f3cdf7b57dea010874712b53440e77f6be1
Vit Troi Fux <vit.fux@protonmail.com>
2021-01-20 17:29:18 +0100

    PIN-1257: added new testing assistances for every modality

--- src/CommonBundle/Controller/CountryController.php
+++ src/CommonBundle/Controller/CountryController.php
@@ -12,28 +12,32 @@ class CountryController extends Controller
 {
     // todo move to DB
     const COUNTRIES = [
-        [
+        'KHM' => [
             'name' => 'Cambodia',
             'iso3' => 'KHM',
             'currency' => 'KHR',
-        ], [
+        ],
+        'SYR' => [
             'name' => 'Syria',
             'iso3' => 'SYR',
             'currency' => 'SYP',
-        ], [
+        ],
+        'UKR' => [
             'name' => 'Ukraine',
             'iso3' => 'UKR',
             'currency' => 'UAH',
-        ], [
+        ],
+        'ETH' => [
             'name' => 'Ethiopia',
             'iso3' => 'ETH',
             'currency' => 'ETB',
-        ], [
+        ],
+        'MNG' => [
             'name' => 'Mongolia',
             'iso3' => 'MNG',
             'currency' => 'MNT',
         ],
-        [
+        'ARM' => [
             'name' => 'Armenia',
             'iso3' => 'ARM',
             'currency' => 'AMD',
