c0b5acf3f19c6401f22c05bf9836ac6ec46f49c3
Jaroslav Bulava <jaroslav.bulava@quanti.cz>
2021-03-02 21:47:53 +0100

    PIN-1328: Added language endpoint

--- src/NewApiBundle/Controller/CommonController.php
+++ src/NewApiBundle/Controller/CommonController.php
@@ -6,7 +6,6 @@ namespace NewApiBundle\Controller;
 
 use BeneficiaryBundle\Entity\Household;
 use CommonBundle\Pagination\Paginator;
-use DistributionBundle\Entity\ModalityType;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -75,6 +74,25 @@ class CommonController extends AbstractController
     }
 
     /**
+     * @Rest\Get("/languages")
+     *
+     * @return JsonResponse
+     */
+    public function languages(): JsonResponse
+    {
+        $data = [];
+
+        foreach ($this->getParameter('app.locales') as $locale) {
+            $data[] = [
+                'code' => $locale,
+                'value' => $this->get('translator')->trans($locale, [], null, $locale),
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+    /**
      * @Rest\Get("/translations/{language}")
      *
      * @param string $language
@@ -83,6 +101,10 @@ class CommonController extends AbstractController
      */
     public function translations(string $language): JsonResponse
     {
+        if (!in_array($language, $this->getParameter('app.locales'))) {
+            throw $this->createNotFoundException('Locale '.$language.' does not exists.');
+        }
+
         $data = [];
 
         foreach ($this->get('translator')->getCatalogue($language)->all('messages') as $key => $value) {
