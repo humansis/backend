77f6848bfbd8f87f222cf8968b69893e71915a56
Jaroslav Bulava <jaroslav.bulava@quanti.cz>
2021-04-02 20:10:53 +0200

    Created object representation of countries and integrate it to new api

---
+++ src/NewApiBundle/Component/Country/Countries.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace NewApiBundle\Component\Country;
+
+class Countries
+{
+    private $array;
+
+    public function __construct(array $array)
+    {
+        $this->array = $array;
+    }
+
+    /**
+     * @return Country[]
+     */
+    public function getAll(): array
+    {
+        return $this->lazyList();
+    }
+
+    /**
+     * @param string $iso3
+     *
+     * @return Country|null
+     */
+    public function getCountry(string $iso3): ?Country
+    {
+        foreach ($this->lazyList() as $country) {
+            if ($iso3 === $country->getIso3()) {
+                return $country;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @return Country[]
+     */
+    private function lazyList()
+    {
+        static $cache;
+
+        if (null === $cache) {
+            $cache = [];
+            foreach ($this->array as $item) {
+                $cache[] = new Country($item);
+            }
+        }
+
+        return $cache;
+    }
+}
