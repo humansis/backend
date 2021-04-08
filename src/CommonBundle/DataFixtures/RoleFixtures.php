c4420a2e7948cc36fe4f500eda6881e722d63692
Jaroslav Bulava <jaroslav.bulava@quanti.cz>
2021-04-08 15:12:48 +0200

    PIN-1569: Added fixtures for roles-privileges

--- src/CommonBundle/DataFixtures/RoleFixtures.php
+++ src/CommonBundle/DataFixtures/RoleFixtures.php
@@ -5,6 +5,7 @@ namespace CommonBundle\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use NewApiBundle\Entity\Privilege;
 use NewApiBundle\Entity\Role;
 
 class RoleFixtures extends Fixture
@@ -39,13 +40,62 @@ class RoleFixtures extends Fixture
         'ROLE_ENUMERATOR',
     ];
 
+    private const PRIVILEGES = [
+        'Add Project' => ['ROLE_ADMIN', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER'],
+        'Edit Project' => ['ROLE_ADMIN', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER'],
+        'Delete Project' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER'],
+        'View Project' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER', 'ROLE_FIELD_OFFICER', 'ROLE_ENUMERATOR'],
+        'Add Distribution' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER'],
+        'Edit Distribution' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER'],
+        'Delete Distribution' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER'],
+        'View Distribution' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER', 'ROLE_FIELD_OFFICER', 'ROLE_ENUMERATOR'],
+        'Assign Distribution Items' => ['ROLE_ADMIN', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER', 'ROLE_FIELD_OFFICER', 'ROLE_ENUMERATOR'],
+        'Add Beneficiary' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER'],
+        'Edit Beneficiary' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER'],
+        'Delete Beneficiary' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER'],
+        'View Beneficiary' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER', 'ROLE_FIELD_OFFICER', 'ROLE_ENUMERATOR'],
+        'Import Beneficiaries' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER'],
+        'Export Beneficiaries' => ['ROLE_ADMIN', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER'],
+        'View Vouchers' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER', 'ROLE_FIELD_OFFICER'],
+        'Export/Print Vouchers' => ['ROLE_ADMIN', 'ROLE_COUNTRY_MANAGER'],
+        'Add Vouchers' => ['ROLE_ADMIN', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER'],
+        'View Vendors' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER', 'ROLE_FIELD_OFFICER'],
+        'Add/Edit Vendors' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER'],
+        'View Donors' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER', 'ROLE_FIELD_OFFICER'],
+        'Add/Edit Donors' => ['ROLE_ADMIN', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER'],
+        'View Products' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER', 'ROLE_FIELD_OFFICER'],
+        'Add/Edit Products' => ['ROLE_ADMIN', 'ROLE_PROJECT_MANAGER'],
+        'Country Report' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER'],
+        'Project Report' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER'],
+        'Distribution Report' => ['ROLE_ADMIN', 'ROLE_REGIONAL_MANAGER', 'ROLE_COUNTRY_MANAGER', 'ROLE_PROJECT_MANAGER', 'ROLE_PROJECT_OFFICER', 'ROLE_FIELD_OFFICER'],
+        'Authorise Electronic Cash Transfer' => ['ROLE_COUNTRY_MANAGER'],
+        'Country Settings' => ['ROLE_ADMIN'],
+        'Add/Edit Users' => ['ROLE_ADMIN'],
+        'Admin Settings' => ['ROLE_ADMIN'],
+    ];
+
     public function load(ObjectManager $manager)
     {
+        $roles = [];
+
         foreach (self::ROLES as $roleName) {
             $role = new Role();
             $role->setName($roleName);
 
+            $roles[$roleName] = $role;
+
             $manager->persist($role);
+        }
+
+        foreach (self::PRIVILEGES as $code => $rls) {
+            $privilege = new Privilege();
+            $privilege->setCode($code);
+
+            foreach ($rls as $r) {
+                $privilege->addRole($roles[$r]);
+            }
+
+            $manager->persist($privilege);
         }
 
         $manager->flush();
