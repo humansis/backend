e1c5ef4493c4481c7a0514e3db81064200734ffd
Jaroslav Bulava <jaroslav.bulava@quanti.cz>
2021-04-17 16:37:04 +0200

    Added transaction endpoints

--- src/NewApiBundle/Controller/TransactionController.php
+++ src/NewApiBundle/Controller/TransactionController.php
@@ -11,6 +11,8 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use NewApiBundle\Component\Codelist\CodeLists;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use TransactionBundle\Entity\Transaction;
 
 class TransactionController extends AbstractController
@@ -31,6 +33,36 @@ class TransactionController extends AbstractController
             ->findByAssistanceBeneficiary($assistance, $beneficiary);
 
         return $this->json(new Paginator($list));
+    }
+
+    /**
+     * @Rest\Post("/assistances/{id}/transactions")
+     *
+     * @param Assistance $assistance
+     * @param Request    $request
+     *
+     * @return JsonResponse
+     */
+    public function createTransactions(Assistance $assistance, Request $request): JsonResponse
+    {
+        $this->forward(\TransactionBundle\Controller\TransactionController::class.'::sendTransactionAction', [$request, $assistance]);
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * @Rest\Post("/assistances/{id}/transactions/emails")
+     * @ParamConverter("assistance", options={"mapping": {"id": "id"}})
+     *
+     * @param Assistance $assistance
+     *
+     * @return JsonResponse
+     */
+    public function sendEmail(Assistance $assistance): JsonResponse
+    {
+        $this->get('transaction.transaction_service')->sendVerifyEmail($this->getUser(), $assistance);
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
     }
 
     /**
