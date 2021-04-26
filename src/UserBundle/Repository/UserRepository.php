079419a32b481440d6f369aa1d2f9bef178743e4
Jaroslav Bulava <jaroslav.bulava@quanti.cz>
2021-04-26 23:21:36 +0200

    Fixed sorting in list of users

--- src/UserBundle/Repository/UserRepository.php
+++ src/UserBundle/Repository/UserRepository.php
@@ -87,7 +87,9 @@ class UserRepository extends \Doctrine\ORM\EntityRepository
                         $qb->orderBy('u.email', $direction);
                         break;
                     case UserOrderInputType::SORT_BY_RIGHTS:
-                        $qb->orderBy('u.roles', $direction); //TODO edit after decision about roles and authorization will be made
+                        $qb
+                            ->join('u.roles', 'r')
+                            ->orderBy('r.name', $direction);
                         break;
                     case UserOrderInputType::SORT_BY_PREFIX:
                         $qb->orderBy('u.phonePrefix', $direction);
