5a9a7d72a2ed3302fc1ba99a1b15760af2b16ecc
Jaroslav Bulava <jaroslav.bulava@quanti.cz>
2021-04-28 23:04:19 +0200

    PIN-1699: Fixed purchased item endpoints

--- src/TransactionBundle/Controller/TransactionController.php
+++ src/TransactionBundle/Controller/TransactionController.php
@@ -13,8 +13,7 @@ use Swagger\Annotations as SWG;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use TransactionBundle\Entity\Transaction;
-use TransactionBundle\Repository\PurchasedItemRepository;
+use TransactionBundle\Entity\PurchasedItem;
 
 /**
  * Class TransactionController
@@ -288,7 +287,7 @@ class TransactionController extends Controller
      */
     public function purchasesAction(Beneficiary $beneficiary)
     {
-        $result = $this->getDoctrine()->getRepository(PurchasedItemRepository::class)->getPurchases($beneficiary);
+        $result = $this->getDoctrine()->getRepository(PurchasedItem::class)->getPurchases($beneficiary);
 
         return $this->json($result);
     }
@@ -308,7 +307,7 @@ class TransactionController extends Controller
      */
     public function purchasesOfHouseholdAction(Household $household)
     {
-        $result = $this->getDoctrine()->getRepository(PurchasedItemRepository::class)->getHouseholdPurchases($household);
+        $result = $this->getDoctrine()->getRepository(PurchasedItem::class)->getHouseholdPurchases($household);
 
         return $this->json($result);
     }
