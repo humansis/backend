d35ea9477f1a7d3c8f7df12570bea749f0c90f94
Pavlix <pavel.kraus01@gmail.com>
2021-05-18 19:40:52 +0200

    PIN-1840: Duplicities endpoint

---
+++ src/NewApiBundle/Repository/ImportBeneficiaryDuplicityRepository.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace NewApiBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use NewApiBundle\Entity\Import;
+
+class ImportBeneficiaryDuplicityRepository extends EntityRepository
+{
+    public function findByImport(Import $import): Paginator
+    {
+        $qbr = $this->createQueryBuilder('ibd')
+            ->leftJoin('ibd.ours', 'importQueue')
+            ->andWhere('importQueue.import = :import')
+            ->setParameter('import', $import);
+
+        return new Paginator($qbr);
+    }
+}
