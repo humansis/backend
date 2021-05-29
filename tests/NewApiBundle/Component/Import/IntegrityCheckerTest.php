5f31d5154ccd919524bf6d8227677b93dc12c931
Vit Troi Fux <vit.fux@protonmail.com>
2021-05-29 14:13:07 +0200

    PIN-1842: extended IntegrityCheckerTest.php

--- tests/NewApiBundle/Component/Import/IntegrityCheckerTest.php
+++ tests/NewApiBundle/Component/Import/IntegrityCheckerTest.php
@@ -8,6 +8,8 @@ use NewApiBundle\Component\Import\IntegrityChecker;
 use NewApiBundle\Entity\Import;
 use NewApiBundle\Entity\ImportFile;
 use NewApiBundle\Entity\ImportQueue;
+use NewApiBundle\Enum\ImportQueueState;
+use NewApiBundle\Enum\ImportState;
 use ProjectBundle\Entity\Project;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -31,7 +33,7 @@ class IntegrityCheckerTest extends KernelTestCase
         self::$entityManager = $kernel->getContainer()->get('doctrine')->getManager();
     }
 
-    public function testParse()
+    public function testParseEmpty()
     {
         $project = self::$entityManager->getRepository(Project::class)->findBy(['archived' => false, 'iso3' => 'KHM'], null, 1)[0];
         $user = self::$entityManager->getRepository(User::class)->findBy([], null, 1)[0];
@@ -47,6 +49,58 @@ class IntegrityCheckerTest extends KernelTestCase
         $method->setAccessible(true);
         $method->invoke($checker, $item);
 
-       $this->assertJson($item->getMessage());
+        $this->assertJson($item->getMessage());
     }
+
+    public function testParseCorrect()
+    {
+        $project = self::$entityManager->getRepository(Project::class)->findBy(['archived' => false, 'iso3' => 'KHM'], null, 1)[0];
+        $user = self::$entityManager->getRepository(User::class)->findBy([], null, 1)[0];
+
+        $import = new Import('test', null, $project, $user);
+        $file = new ImportFile('fake_file.xlsx', $import, $user);
+
+        $item = new ImportQueue($import, $file, json_decode(ImportFinishServiceTest::TEST_QUEUE_ITEM, true));
+
+        $checker = new IntegrityChecker(self::$validator, self::$entityManager);
+
+        $method = new \ReflectionMethod($checker, 'checkOne');
+        $method->setAccessible(true);
+        $method->invoke($checker, $item);
+
+        $this->assertNull($item->getMessage());
+    }
+
+    public function testCheck()
+    {
+        $project = self::$entityManager->getRepository(Project::class)->findBy(['archived' => false, 'iso3' => 'KHM'], null, 1)[0];
+        $user = self::$entityManager->getRepository(User::class)->findBy([], null, 1)[0];
+
+        $import = new Import('test', null, $project, $user);
+        $import->setState(ImportState::INTEGRITY_CHECKING);
+
+        $file = new ImportFile('fake_file.xlsx', $import, $user);
+        $item = new ImportQueue($import, $file, json_decode(ImportFinishServiceTest::TEST_QUEUE_ITEM, true));
+        self::$entityManager->persist($import);
+        self::$entityManager->persist($file);
+        self::$entityManager->persist($item);
+        self::$entityManager->flush();
+
+        $checker = new IntegrityChecker(self::$validator, self::$entityManager);
+        $checker->check($import);
+
+        $queue = self::$entityManager->getRepository(\NewApiBundle\Entity\ImportQueue::class)->findBy(['import' => $import]);
+        $this->assertCount(1, $queue);
+        foreach ($queue as $item) {
+            $this->assertEquals(ImportQueueState::VALID, $item->getState(), "Queue is invalid because ".$item->getMessage());
+        }
+        $this->assertEquals(ImportState::INTEGRITY_CHECK_CORRECT, $import->getState());
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        self::$entityManager->clear();
+    }
+
 }
