8692bf2140867595a3fbe309ef1adb5a6458b249
Pavlix <pavel.kraus01@gmail.com>
2021-06-27 23:53:42 +0200

    PIN-2052: Import implementation

--- src/NewApiBundle/Command/WingMoneyReportImportCommand.php
+++ src/NewApiBundle/Command/WingMoneyReportImportCommand.php
@@ -4,40 +4,89 @@ declare(strict_types=1);
 namespace NewApiBundle\Command;
 
 use DistributionBundle\Entity\Assistance;
-use DistributionBundle\Repository\AssistanceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use InvalidArgumentException;
+use NewApiBundle\Component\WingMoney\ImportService;
+use NewApiBundle\Component\WingMoney\ReportParser;
+use NewApiBundle\Component\WingMoney\ValueObject\ReportEntry;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use UserBundle\Entity\User;
 
 class WingMoneyReportImportCommand extends Command
 {
     /**
-     * @var AssistanceRepository
+     * @var EntityManagerInterface
      */
-    private $assistanceRepository;
+    private $em;
 
-    public function __construct(AssistanceRepository $assistanceRepository, string $name = null)
+    /**
+     * @var ReportParser
+     */
+    private $reportParser;
+
+    /**
+     * @var ImportService
+     */
+    private $importService;
+
+    public function __construct(EntityManagerInterface $em, ReportParser $reportParser, ImportService $importService, string $name = null)
     {
         parent::__construct($name);
 
-        $this->assistanceRepository = $assistanceRepository;
+        $this->em = $em;
+        $this->reportParser = $reportParser;
+        $this->importService = $importService;
     }
-
 
     public function configure()
     {
         $this->setName('app:wing-money:import')
             ->addArgument('reportFile', InputArgument::REQUIRED, 'Report file in xlsx format')
-            ->addArgument('assistance', InputArgument::REQUIRED, 'ID of an assistance in which the transactions will be imported');
+            ->addArgument('assistance', InputArgument::REQUIRED, 'ID of an assistance in which the transactions will be imported')
+            ->addArgument('user',InputArgument::REQUIRED, 'ID of an user. Will be saved as sendBy for transactions.')
+            ->addOption('check');
     }
 
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output): int
     {
         $reportFile = $this->getReportFilePath($input);
         $assistance = $this->getAssistance($input);
+        $user = $this->getUser($input);
 
+        echo 'Parsing file "'.$reportFile.'"'.PHP_EOL;
+
+        $entries = $this->reportParser->parseEntries($reportFile);
+
+        $totalEntries = count($entries);
+        echo 'Found '.$totalEntries.' valid entries in given file'.PHP_EOL;
+
+        $newEntries = $this->importService->filterExistingTransactions($entries);
+        $totalInSystem = $totalEntries - count($newEntries);
+
+        echo $totalInSystem.' entries are already in system'.PHP_EOL;
+
+        $entriesToImport = $this->importService->filterTransactionsInAssistanceOnly($newEntries, $assistance);
+
+        echo count($newEntries) - count($entriesToImport).' entries won\'t be imported (phone number not found, phone number does not belong to beneficiary in given assistance, amount does not match with assistance commodity)'.PHP_EOL;
+        echo count($entriesToImport).' entries ready to import'.PHP_EOL;
+
+        /** @var ReportEntry $entry */
+        foreach ($entriesToImport as $entry) {
+            if (!$input->getOption('check')) {
+                $this->importService->createTransactionFromReportEntry($entry, $assistance, $user);
+            }
+        }
+
+        if ($input->getOption('check')) {
+            echo PHP_EOL.'None entries were imported.'.PHP_EOL;
+        } else {
+            echo PHP_EOL.'Entries were imported'.PHP_EOL;
+        }
+
+        return 0;
     }
 
 
@@ -62,12 +111,26 @@ class WingMoneyReportImportCommand extends Command
         $assistanceId = $input->getArgument('assistance');
 
         /** @var Assistance|null $assistance */
-        $assistance = $this->assistanceRepository->find($assistanceId);
+        $assistance = $this->em->getRepository(Assistance::class)->find($assistanceId);
 
         if (!$assistance instanceof Assistance) {
             throw new InvalidArgumentException("Assistance wit ID $assistanceId does not exist");
         }
 
         return $assistance;
+    }
+
+    private function getUser(InputInterface $input): User
+    {
+        $userId = $input->getArgument('user');
+
+        /** @var User|null $user */
+        $user = $this->em->getRepository(User::class)->find($userId);
+
+        if (!$user instanceof User) {
+            throw new InvalidArgumentException("User wit ID $userId does not exist");
+        }
+
+        return $user;
     }
 }
