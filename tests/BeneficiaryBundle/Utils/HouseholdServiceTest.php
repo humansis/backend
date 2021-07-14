12a4e925b79e132a7103dfa47d57adbb81d1c517
Vit Troi Fux <vit.fux@protonmail.com>
2021-07-14 14:51:17 +0200

    PIN-2057: test phones

--- tests/BeneficiaryBundle/Utils/HouseholdServiceTest.php
+++ tests/BeneficiaryBundle/Utils/HouseholdServiceTest.php
@@ -10,6 +10,7 @@ use NewApiBundle\Component\Import\ImportService;
 use NewApiBundle\InputType\Beneficiary\Address\ResidenceAddressInputType;
 use NewApiBundle\InputType\Beneficiary\AddressInputType;
 use NewApiBundle\InputType\Beneficiary\BeneficiaryInputType;
+use NewApiBundle\InputType\Beneficiary\PhoneInputType;
 use NewApiBundle\InputType\HouseholdCreateInputType;
 use NewApiBundle\InputType\HouseholdUpdateInputType;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
@@ -48,9 +49,9 @@ class HouseholdServiceTest extends KernelTestCase
         $createData->setProjectIds([1]);
         $createData->setIso3('KHM');
         $createData->setAssets(["3", 2]);
-        $createData->setLongitude('');
-        $createData->setLatitude('');
-        $createData->setNotes('');
+        $createData->setLongitude('12.123456');
+        $createData->setLatitude('54.321');
+        $createData->setNotes('Lorem ipsum');
 
         $addressData = new ResidenceAddressInputType();
         $addressData->setLocationId(1);
@@ -63,10 +64,17 @@ class HouseholdServiceTest extends KernelTestCase
         $createBeneficiary->setGender('F');
         $createBeneficiary->setDateOfBirth('2000-01-01');
         $createBeneficiary->setIsHead(true);
-        $createBeneficiary->setLocalGivenName('test');
-        $createBeneficiary->setLocalFamilyName('test');
+        $createBeneficiary->setLocalGivenName('testGiven');
+        $createBeneficiary->setLocalFamilyName('testFamily');
         $createBeneficiary->setResidencyStatus(ResidencyStatus::RESIDENT);
         $createData->addBeneficiary($createBeneficiary);
+
+        $phone = new PhoneInputType();
+        $phone->setPrefix('+855');
+        $phone->setType('Mobile');
+        $phone->setProxy(true);
+        $phone->setNumber('123 456 789');
+        $createBeneficiary->addPhone($phone);
 
         $violations = $this->validator->validate($createData);
         if ($violations->count() > 0) {
@@ -74,12 +82,35 @@ class HouseholdServiceTest extends KernelTestCase
             foreach ($violations as $violation) {
                 echo "[{$violation->getPropertyPath()} = '{$violation->getInvalidValue()}'] {$violation->getMessage()}\n";
             }
-            $this->fail('Testing data are wrong');
+            $this->fail('Testing data are invalid');
         }
 
         $household = $this->householdService->create($createData);
         $this->assertNotNull($household);
         $this->assertNotNull($household->getId());
+        $this->assertEquals('12.123456', $household->getLongitude());
+        $this->assertEquals('54.321', $household->getLatitude());
+        $this->assertEquals('Lorem ipsum', $household->getNotes());
+        $this->assertCount(2, $household->getAssets());
+        $this->assertCount(1, $household->getProjects());
+        $this->assertEquals(1, $household->getProjects()[0]->getId());
+        $this->assertEquals('KHM', $household->getProjects()[0]->getIso3());
+        $this->assertContains(2, $household->getAssets());
+        $this->assertContains(3, $household->getAssets());
+
+        $head = $household->getHouseholdHead();
+        $this->assertEquals('testFamily', $head->getPerson()->getLocalFamilyName());
+        $this->assertEquals('testGiven', $head->getPerson()->getLocalGivenName());
+        $this->assertNull($head->getPerson()->getEnGivenName());
+        $this->assertNull($head->getPerson()->getEnFamilyName());
+        $this->assertNull($head->getPerson()->getEnParentsName());
+
+        $phones = $head->getPerson()->getPhones();
+        $this->assertCount(1, $phones, "Wrong phone count");
+        $this->assertEquals('+855', $phones[0]->getPrefix());
+        $this->assertEquals('123 456 789', $phones[0]->getNumber());
+        $this->assertEquals('Mobile', $phones[0]->getType());
+        $this->assertTrue($phones[0]->getProxy());
 
         return $household->getId();
     }
