7e3d921b02a9170df9e0a8d2251d03403af75516
Pavlix <pavel.kraus01@gmail.com>
2021-08-30 11:37:37 +0200

    lower amount of data processed by cron #2

--- src/NewApiBundle/Command/Import/AbstractImportQueueCommand.php
+++ src/NewApiBundle/Command/Import/AbstractImportQueueCommand.php
@@ -25,7 +25,7 @@ abstract class AbstractImportQueueCommand extends Command
     /** @var ImportService */
     protected $importService;
     /** @var int */
-    protected $batchSize = 200;
+    protected $batchSize = 10;
 
     /**
      * AbstractImportQueueCommand constructor.
