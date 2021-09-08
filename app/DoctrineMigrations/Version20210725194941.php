58a24f147bb7edc5c69f13e2165a56e2fad431a8
Vit Troi Fux <vit.fux@protonmail.com>
2021-09-08 09:26:12 +0200

    PIN-2139: fix ProductCategory DB

--- app/DoctrineMigrations/Version20210725194941.php
+++ app/DoctrineMigrations/Version20210725194941.php
@@ -15,7 +15,7 @@ final class Version20210725194941 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE product_category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql("CREATE TABLE product_category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, type enum('Food','Non-food','Cashback') NOT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB");
         $this->addSql('ALTER TABLE product ADD product_category_id INT DEFAULT NULL, ADD CONSTRAINT FK_D34A04ADBE6903FD FOREIGN KEY (product_category_id) REFERENCES product_category (id)');
 
         $categories = [
@@ -23,8 +23,9 @@ final class Version20210725194941 extends AbstractMigration
             'Non-food',
             'Cashback',
         ];
-
-        $this->addSql('INSERT INTO product_category (name) VALUES(\'' . implode('\'), (\'', $categories) . '\')');
+        foreach ($categories as $category) {
+            $this->addSql("INSERT INTO product_category (name, type) VALUES('$category', '$category')");
+        }
     }
 
     public function down(Schema $schema) : void
