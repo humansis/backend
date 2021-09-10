e345b437072058ed89cfee2034ee2e56133412a4
Vit Troi Fux <vit.fux@protonmail.com>
2021-09-10 13:52:11 +0200

    PIN-2139: set all vendors to sell only food

---
+++ app/DoctrineMigrations/Version20210910115019.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210910115019 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('UPDATE `vendor` SET `can_sell_food` = 1, `can_sell_non_food` = 0, `can_sell_cashback` = 0');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
