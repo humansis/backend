e2f18bc11a368d9cc2932b16893b504408b8220d
Vit Troi Fux <vit.fux@protonmail.com>
2021-09-25 16:51:08 +0200

    added legacy API versions of UserApp and VendorApp

--- docker/scripts/generate-api.sh
+++ docker/scripts/generate-api.sh
@@ -20,5 +20,16 @@ docker run --rm \
   -c appdir/app/config/generator-userapp-api.yml \
   -o appdir/vendor/humansis/user-app-api-sdk
 
-#docker run --rm \
-#  -v "${PWD}":/appdir openapitools/openapi-generator-cli config-help -g php-symfony -f yamlsample
+docker run --rm \
+  -v "${PWD}":/appdir openapitools/openapi-generator-cli generate \
+  -i appdir/vendor/humansis/vendor-app-legacy-api/swagger.yaml \
+  -g php-symfony \
+  -c appdir/app/config/generator-vendorapp-legacy-api.yml \
+  -o appdir/vendor/humansis/vendor-app-legacy-api-sdk
+
+docker run --rm \
+  -v "${PWD}":/appdir openapitools/openapi-generator-cli generate \
+  -i appdir/vendor/humansis/user-app-legacy-api/swagger.yaml \
+  -g php-symfony \
+  -c appdir/app/config/generator-userapp-legacy-api.yml \
+  -o appdir/vendor/humansis/user-app-legacy-api-sdk
