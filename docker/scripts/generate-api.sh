5b313b32c1294b06b9ba09ddb9a77c240cb17ba7
Vit Troi Fux <vit.fux@protonmail.com>
2021-09-25 16:15:16 +0200

    fixed generating VendorAppApiSdk

--- docker/scripts/generate-api.sh
+++ docker/scripts/generate-api.sh
@@ -6,5 +6,12 @@ docker run --rm \
   -c appdir/app/config/generator-webapi.yml \
   -o appdir/vendor/humansis/web-api-sdk
 
+docker run --rm \
+  -v "${PWD}":/appdir openapitools/openapi-generator-cli generate \
+  -i appdir/vendor/humansis/vendor-app-api/swagger.yaml \
+  -g php-symfony \
+  -c appdir/app/config/generator-vendorapp-api.yml \
+  -o appdir/vendor/humansis/vendor-app-api-sdk
+
 #docker run --rm \
 #  -v "${PWD}":/appdir openapitools/openapi-generator-cli config-help -g php-symfony -f yamlsample
