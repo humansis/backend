f05d459b0720382463ae9251aebbe8ae728c78b3
Pavlix <pavel.kraus01@gmail.com>
2021-10-12 16:38:13 +0200

    PIN-2192: Rename ABC to ReliefPackage in deposit

--- src/VoucherBundle/Utils/SmartcardService.php
+++ src/VoucherBundle/Utils/SmartcardService.php
@@ -95,7 +95,7 @@ class SmartcardService
         $deposit = SmartcardDeposit::create(
             $smartcard,
             $user,
-            $assistanceBeneficiaryCommodity,
+            $reliefPackage,
             (float) $value,
             null !== $balance ? (float) $balance : null,
             $distributedAt
