4ae4f67c06c4085a4a8028b3fa65e1a6ffa784d8
Vit Troi Fux <vit.fux@protonmail.com>
2021-10-25 18:13:26 +0200

    generate only models

--- docker/scripts/generate-api.sh
+++ docker/scripts/generate-api.sh
@@ -4,32 +4,37 @@ docker run --rm \
   -i appdir/vendor/humansis/web-api/swagger.yaml \
   -g php-symfony \
   -c appdir/app/config/generator-webapi.yml \
-  -o appdir/src/HumansisWebApiBundle
+  -o appdir/src/HumansisWebApiBundle \
+  --global-property models
 
 docker run --rm \
   -v "${PWD}":/appdir openapitools/openapi-generator-cli generate \
   -i appdir/vendor/humansis/vendor-app-api/swagger.yaml \
   -g php-symfony \
   -c appdir/app/config/generator-vendorapp-api.yml \
-  -o appdir/src/HumansisVendorAppApiBundle
+  -o appdir/src/HumansisVendorAppApiBundle \
+  --global-property models
 
 docker run --rm \
   -v "${PWD}":/appdir openapitools/openapi-generator-cli generate \
   -i appdir/vendor/humansis/user-app-api/swagger.yaml \
   -g php-symfony \
   -c appdir/app/config/generator-userapp-api.yml \
-  -o appdir/src/HumansisUserAppApiBundle
+  -o appdir/src/HumansisUserAppApiBundle \
+  --global-property models
 
 docker run --rm \
   -v "${PWD}":/appdir openapitools/openapi-generator-cli generate \
   -i appdir/vendor/humansis/vendor-app-legacy-api/swagger.yaml \
   -g php-symfony \
   -c appdir/app/config/generator-vendorapp-legacy-api.yml \
-  -o appdir/src/HumansisLegacyVendorAppApiBundle
+  -o appdir/src/HumansisLegacyVendorAppApiBundle \
+  --global-property models
 
 docker run --rm \
   -v "${PWD}":/appdir openapitools/openapi-generator-cli generate \
   -i appdir/vendor/humansis/user-app-legacy-api/swagger.yaml \
   -g php-symfony \
   -c appdir/app/config/generator-userapp-legacy-api.yml \
-  -o appdir/src/HumansisLegacyUserAppApiBundle
+  -o appdir/src/HumansisLegacyUserAppApiBundle \
+  --global-property models
