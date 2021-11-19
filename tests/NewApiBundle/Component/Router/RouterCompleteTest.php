481c6b55586c0374cac500c77084f453bf11d325
Vit Troi Fux <vit.fux@protonmail.com>
2021-11-19 10:52:36 +0100

    move sgw endpoints to data provider

--- tests/NewApiBundle/Component/Router/RouterCompleteTest.php
+++ tests/NewApiBundle/Component/Router/RouterCompleteTest.php
@@ -18,6 +18,13 @@ class RouterCompleteTest extends KernelTestCase
     private static $swaggerEndpoints = [];
 
     const SWG_IGNORE_METHODS = ['PARAMETERS'];
+    const SWG_FILES = [
+        ['prefix' => '/api/{firewall}/web-app', 'path' => 'vendor/humansis/web-api/swagger.yaml'],
+        ['prefix' => '/api/{firewall}/vendor-app', 'path' => 'vendor/humansis/vendor-app-api/swagger.yaml'],
+        ['prefix' => '/api/{firewall}/offline-app', 'path' => 'vendor/humansis/user-app-api/swagger.yaml'],
+        ['prefix' => '/api/{firewall}/vendor-app', 'path' => 'vendor/humansis/vendor-app-legacy-api/swagger.yaml'],
+        ['prefix' => '/api/{firewall}/offline-app', 'path' => 'vendor/humansis/user-app-legacy-api/swagger.yaml'],
+    ];
 
     public static function setUpBeforeClass()
     {
@@ -35,35 +42,26 @@ class RouterCompleteTest extends KernelTestCase
                 self::$applicationEndpoints[$path][] = strtoupper($method);
             }
         }
-
-        $swaggers = $kernel->getContainer()->getParameter('app.swaggers');
-        foreach ($swaggers as $swaggerConfig) {
+    }
+    
+    public function swaggerEndpointProvider(): iterable
+    {
+        foreach (self::SWG_FILES as $swaggerConfig) {
             $swagger = Yaml::parse(file_get_contents($swaggerConfig['path']));
             foreach ($swagger['paths'] as $path => $pathDescription) {
                 foreach ($pathDescription as $method => $pathMethodDescription) {
                     $method = strtoupper($method);
                     $normalizedPath = strtolower($swaggerConfig['prefix'].$path);
+                    // ignore path parameter which aren't methods
                     if (in_array($method, self::SWG_IGNORE_METHODS)) continue;
+                    // ignore method duplicities between swaggers
                     if (isset(self::$swaggerEndpoints[$normalizedPath]) && in_array($method, self::$swaggerEndpoints[$normalizedPath])) continue;
+
                     self::$swaggerEndpoints[$normalizedPath][] = $method;
+                    yield $pathMethodDescription['summary'] ?? $normalizedPath => [$normalizedPath, $method];
                 }
             }
         }
-    }
-
-    public function testApplicationEndpointsAreComplete()
-    {
-        $correct = 0;
-        $failed = 0;
-        // TODO: move to dataprovider
-        foreach (self::$swaggerEndpoints as $path => $methods) {
-            foreach ($methods as $method) {
-                $found = $this->assertAppHasEndpoint($path, $method);
-                if ($found) $correct++; else $failed++;
-            }
-        }
-        $all = $correct + $failed;
-        $this->assertEquals(0, $failed, "There are $failed unimplemented endpoints ($all endpoint is documented)");
     }
 
     public function testSwaggersAreComplete()
@@ -81,15 +79,15 @@ class RouterCompleteTest extends KernelTestCase
         $this->assertEquals(0, $failed, "There are $failed undocumented endpoints ($all endpoint is implemented)");
     }
 
-    private function assertAppHasEndpoint(string $path, string $method): bool
+    /**
+     * @dataProvider swaggerEndpointProvider
+     * @param string $path
+     * @param string $method
+     */
+    public function testAppHasEndpoint(string $path, string $method): void
     {
-        if (!array_key_exists($path, self::$applicationEndpoints) || !in_array($method, self::$applicationEndpoints[$path])) {
-            echo "Application missing endpoint with path $path [$method]\n";
-            return false;
-        }
-        // $this->assertArrayHasKey($path, self::$applicationEndpoints, "Application missing endpoint with path $path");
-        // $this->assertContains($method, self::$applicationEndpoints[$path], "Application missing method $method for endpoint with path $path");
-        return true;
+        $this->assertArrayHasKey($path, self::$applicationEndpoints, "Application missing endpoint with path $path");
+        $this->assertContains($method, self::$applicationEndpoints[$path], "Application missing method $method for endpoint with path $path");
     }
 
     private function assertSwgHasEndpoint(string $path, string $method): bool
