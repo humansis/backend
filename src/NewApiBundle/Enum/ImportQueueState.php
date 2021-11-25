13d68cb714611571c9501bccf9511313dcd3ab4a
Lukas Kral <luke.kral@seznam.cz>
2021-11-25 11:14:37 +0100

    PIN-2554: Migrations for new Queue states

--- src/NewApiBundle/Enum/ImportQueueState.php
+++ src/NewApiBundle/Enum/ImportQueueState.php
@@ -9,11 +9,17 @@ final class ImportQueueState
     const VALID = 'Valid';
     const INVALID = 'Invalid';
     const INVALID_EXPORTED = 'Invalid Exported';
+    const IDENTITY_CANDIDATE = 'Identity Candidate';
+    const UNIQUE_CANDIDATE = 'Unique Candidate';
+    const SIMILARITY_CANDIDATE = 'Similarity Candidate';
     const SUSPICIOUS = 'Suspicious';
     const TO_CREATE = 'To Create';
     const TO_UPDATE = 'To Update';
     const TO_LINK = 'To Link';
     const TO_IGNORE = 'To Ignore';
+    const CREATED = 'Created';
+    const UPDATED = 'Updated';
+    const LINKED = 'Linked';
 
     public static function values(): array
     {
@@ -22,11 +28,16 @@ final class ImportQueueState
             self::VALID,
             self::INVALID,
             self::INVALID_EXPORTED,
-            self::SUSPICIOUS,
+            self::IDENTITY_CANDIDATE,
+            self::UNIQUE_CANDIDATE,
+            self::SIMILARITY_CANDIDATE,
             self::TO_CREATE,
             self::TO_UPDATE,
             self::TO_LINK,
             self::TO_IGNORE,
+            self::CREATED,
+            self::UPDATED,
+            self::LINKED,
         ];
     }
 
