9797774a8f04b80943633c55000bc4b2accf1bb3
Lukas Kral <luke.kral@seznam.cz>
2021-12-03 12:46:07 +0100

    PIN-2724: Assistance detail - sort by distribution date

--- src/DistributionBundle/Repository/AssistanceBeneficiaryRepository.php
+++ src/DistributionBundle/Repository/AssistanceBeneficiaryRepository.php
@@ -12,6 +12,7 @@ use BeneficiaryBundle\Entity\Household;
 use DistributionBundle\Enum\AssistanceTargetType;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use InvalidArgumentException;
+use NewApiBundle\Entity\ReliefPackage;
 use NewApiBundle\InputType\BeneficiaryFilterInputType;
 use NewApiBundle\InputType\BeneficiaryOrderInputType;
 use NewApiBundle\InputType\CommunityFilterType;
@@ -20,6 +21,7 @@ use NewApiBundle\InputType\InstitutionFilterInputType;
 use NewApiBundle\InputType\InstitutionOrderInputType;
 use NewApiBundle\Request\Pagination;
 use VoucherBundle\Entity\Booklet;
+use VoucherBundle\Entity\SmartcardDeposit;
 
 /**
  * AssistanceBeneficiaryRepository
@@ -204,6 +206,12 @@ class AssistanceBeneficiaryRepository extends \Doctrine\ORM\EntityRepository
                         $qb->leftJoin('p.nationalIds', 'n', 'WITH', 'n.idType = :type')
                             ->setParameter('type', \BeneficiaryBundle\Entity\NationalId::TYPE_NATIONAL_ID)
                             ->orderBy('n.idNumber', $direction);
+                        break;
+                    case BeneficiaryOrderInputType::SORT_BY_DISTRIBUTION_DATE:
+                        $qb
+                            ->leftJoin(ReliefPackage::class, 'reliefPackage', 'WITH', 'reliefPackage.assistanceBeneficiary = db.id')
+                            ->leftJoin(SmartcardDeposit::class, 'smartcardDeposit', 'WITH', 'smartcardDeposit.reliefPackage = reliefPackage.id')
+                            ->orderBy('smartcardDeposit.distributedAt', $direction);
                         break;
                     default:
                         throw new \InvalidArgumentException('Invalid order by directive '.$name);
