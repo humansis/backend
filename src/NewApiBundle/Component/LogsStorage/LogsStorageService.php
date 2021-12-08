72a075b8360336d1cc6e07823f21a83818aeecb3
Pavlix <pavel.kraus01@gmail.com>
2021-12-08 11:18:44 +0100

    PIN-2226: Upload logs of vendor and field app

--- src/NewApiBundle/Component/LogsStorage/LogsStorageService.php
+++ src/NewApiBundle/Component/LogsStorage/LogsStorageService.php
@@ -2,44 +2,51 @@
 
 namespace NewApiBundle\Component\LogsStorage;
 
+use DateTime;
 use League\Flysystem\FilesystemException;
-use NewApiBundle\Component\Storage\IStorage;
-use NewApiBundle\Component\Storage\IStorageConfig;
+use NewApiBundle\Component\Storage\Aws\AwsStorage;
 use NewApiBundle\Factory\AwsStorageFactory;
 use NewApiBundle\Factory\LogsStorageConfigFactory;
+use NewApiBundle\Utils\FileSystem\PathConstructor;
+use NewApiBundle\Utils\FileSystem\Zip;
+use Symfony\Component\HttpFoundation\File\File;
+use UserBundle\Entity\User;
+use VoucherBundle\Entity\Vendor;
 
 class LogsStorageService
 {
+    /**
+     * @var AwsStorage
+     */
+    private $aws;
 
     /**
      * @var string
      */
-    private $folder;
+    private $vendorLogPathTemple;
 
     /**
-     * @var AwsStorageFactory
+     * @var string
      */
-    private $awsStorageFactory;
+    private $fieldLogPathTemplate;
 
     /**
-     * @var IStorageConfig
+     * @var int
      */
-    private $logsStorageConfig;
-
-    /**
-     * @var IStorage
-     */
-    private $aws;
+    private $logsLifetime;
 
     public function __construct(
-        string                   $folder,
+        string $vendorLogPathTemplate,
+        string $filedLogPathTemplate,
+        int $logsLifetime,
         LogsStorageConfigFactory $logsStorageFactory,
         AwsStorageFactory        $awsStorageFactory
     ) {
-        $this->folder = $folder;
-        $this->awsStorageFactory = $awsStorageFactory;
-        $this->logsStorageConfig = $logsStorageFactory->create();
-        $this->aws = $this->awsStorageFactory->create($this->logsStorageConfig);
+        $this->vendorLogPathTemple = $vendorLogPathTemplate;
+        $this->fieldLogPathTemplate = $filedLogPathTemplate;
+        $this->logsLifetime = $logsLifetime;
+
+        $this->aws = $awsStorageFactory->create($logsStorageFactory->create());
     }
 
     /**
@@ -51,8 +58,70 @@ class LogsStorageService
      */
     private function upload(string $fileName, $file): string
     {
-        $path = $this->folder.'/'.$fileName;
+        $path = 'test/'.$fileName;
 
         return $this->aws->upload($path, $file);
+    }
+
+    /**
+     * @param Vendor $vendor
+     * @param File   $file
+     *
+     * @throws FilesystemException
+     */
+    public function uploadVendorApp(Vendor $vendor, File $file)
+    {
+        $extractedZip = Zip::extractToTempDir($file);
+
+        $path = PathConstructor::construct($this->vendorLogPathTemple, [
+            'vendorId' => $vendor->getId(),
+            'datetime' => (new DateTime())->format('Y-m-d_H:i:s'),
+        ]);
+
+        foreach ($extractedZip as $fileFromZip => $extractedPath) {
+            $this->aws->upload($path . $fileFromZip, file_get_contents($extractedPath));
+        }
+    }
+
+    /**
+     * @param User $user
+     * @param File $file
+     *
+     * @throws FilesystemException
+     */
+    public function uploadFieldApp(User $user, File $file)
+    {
+        $extractedZip = Zip::extractToTempDir($file);
+
+        $path = PathConstructor::construct($this->fieldLogPathTemplate, [
+            'userId' => $user->getId(),
+            'datetime' => (new DateTime())->format('Y-m-d_H:i:s'),
+        ]);
+
+        foreach ($extractedZip as $fileFromZip => $extractedPath) {
+            $this->aws->upload($path . $fileFromZip, file_get_contents($extractedPath));
+        }
+    }
+
+    /**
+     * @throws FilesystemException
+     *
+     * @returns array of deleted files
+     */
+    public function clearOldLogs(): array
+    {
+        $deleteBefore = (new DateTime())->modify("- $this->logsLifetime days");
+
+        $oldLogs = $this->aws->listModifiedBefore($deleteBefore);
+
+        $deletedLogsPath = [];
+
+        foreach ($oldLogs as $oldLog) {
+            $this->aws->delete($oldLog->path());
+
+            $deletedLogsPath[] = $oldLog->path();
+        }
+
+        return $deletedLogsPath;
     }
 }
