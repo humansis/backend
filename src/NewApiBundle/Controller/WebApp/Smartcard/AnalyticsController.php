763d69968b592133d2561899c7445a84054a7889
Vít Fux <vit.fux@quanti.cz>
2022-02-18 11:48:05 +0100

    PIN-2906: added information for smartcards

--- src/NewApiBundle/Controller/WebApp/Smartcard/AnalyticsController.php
+++ src/NewApiBundle/Controller/WebApp/Smartcard/AnalyticsController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use VoucherBundle\Entity\Smartcard;
 use VoucherBundle\Entity\Vendor;
 use VoucherBundle\Repository\SmartcardPurchaseRepository;
+use VoucherBundle\Repository\SmartcardRepository;
 
 class AnalyticsController extends AbstractWebAppController
 {
@@ -95,7 +96,9 @@ class AnalyticsController extends AbstractWebAppController
      */
     public function smartcardById(Smartcard $smartcard): JsonResponse
     {
-        return $this->json([]);
+        $collector = new EventCollector();
+        $this->collectSmartcard($collector, $smartcard);
+        return $this->json($collector->getSortedEvents());
     }
 
     /**
@@ -105,9 +108,74 @@ class AnalyticsController extends AbstractWebAppController
      *
      * @return JsonResponse
      */
-    public function smartcardBySerialNumber(string $serialNumber): JsonResponse
+    public function smartcardBySerialNumber(string $serialNumber, SmartcardRepository $smartcardRepository): JsonResponse
     {
-        return $this->json([]);
+        $smartcards = $smartcardRepository->findBy(['serialNumber'=>$serialNumber]);
+        $collector = new EventCollector();
+        foreach ($smartcards as $smartcard) {
+            $this->collectSmartcard($collector, $smartcard);
+        }
+        return $this->json($collector->getSortedEvents());
+    }
+
+    private function collectSmartcard(EventCollector $collector, Smartcard $smartcard): void
+    {
+        foreach ($smartcard->getDeposites() as $deposit) {
+            $reliefPackage = $deposit->getReliefPackage();
+            $assistanceBeneficiary = $reliefPackage->getAssistanceBeneficiary();
+            $assistance = $assistanceBeneficiary->getAssistance();
+
+            $collector->add(new Event('assistance', 'set to distribute', $reliefPackage->getCreatedAt(), [
+                'assistance_id' => $assistance->getId(),
+                'assistance_name' => $assistance->getName(),
+                'relief_package' => $reliefPackage->getId(),
+                'assistance_beneficiary_id' => $assistanceBeneficiary->getId(),
+            ]));
+
+            $collector->add(new Event('assistance', 'started', $assistance->getDateDistribution(), [
+                'assistance_id' => $assistance->getId(),
+                'assistance_name' => $assistance->getName(),
+                'assistance_beneficiary_id' => $assistanceBeneficiary->getId(),
+            ]));
+            if ($assistance->getDateExpiration()) {
+                $collector->add(new Event('assistance', 'expired', $assistance->getDateExpiration(), [
+                    'assistance_id' => $assistance->getId(),
+                    'assistance_name' => $assistance->getName(),
+                    'assistance_beneficiary_id' => $assistanceBeneficiary->getId(),
+                ]));
+            }
+
+            $collector->add(new Event('deposit', 'sync', $deposit->getCreatedAt(), [
+                'deposit_id' => $deposit->getId(),
+            ]));
+
+            $collector->add(new Event('deposit', 'got money', $deposit->getDistributedAt(), [
+                'value' => $deposit->getValue(),
+                'assistance_id' => $assistance->getId(),
+                'assistance_name' => $assistance->getName(),
+                'deposit_id' => $deposit->getId(),
+                'smartcard_id' => $smartcard->getId(),
+                'smartcard_serialNumber' => $deposit->getSmartcard()->getSerialNumber(),
+            ]));
+        }
+
+        foreach ($smartcard->getPurchases() as $purchase) {
+            $collector->add(new Event('purchase', 'made', $purchase->getCreatedAt(), [
+                'value' => $purchase->getRecordsValue().' '.$purchase->getCurrency(),
+                'assistance_id' => $purchase->getAssistance() ? $purchase->getAssistance()->getId() : null,
+                'assistance_name' => $purchase->getAssistance() ? $purchase->getAssistance()->getName() : null,
+                'purchase_id' => $purchase->getId(),
+                'smartcard_id' => $purchase->getSmartcard()->getId(),
+                'smartcard_serialNumber' => $purchase->getSmartcard()->getSerialNumber(),
+                'vendor_id' => $purchase->getVendor()->getId(),
+                'vendor_name' => $purchase->getVendor()->getName(),
+            ]));
+            if ($purchase->getRedemptionBatch()) {
+                $collector->add(new Event('purchase', 'invoiced', $purchase->getRedeemedAt(), [
+                    'purchase_id' => $purchase->getId(),
+                ]));
+            }
+        }
     }
 
     /**
