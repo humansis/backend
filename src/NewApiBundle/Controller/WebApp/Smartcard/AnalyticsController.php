5f6c4214b93a70d048f94e9fad51d98819ab4068
Vít Fux <vit.fux@quanti.cz>
2022-02-18 12:16:40 +0100

    PIN-2906: added information for vendors

--- src/NewApiBundle/Controller/WebApp/Smartcard/AnalyticsController.php
+++ src/NewApiBundle/Controller/WebApp/Smartcard/AnalyticsController.php
@@ -7,10 +7,14 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use NewApiBundle\Component\Smartcard\Analytics\Event;
 use NewApiBundle\Component\Smartcard\Analytics\EventCollector;
 use NewApiBundle\Controller\WebApp\AbstractWebAppController;
+use NewApiBundle\Entity\SynchronizationBatch;
+use NewApiBundle\Enum\SourceType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use VoucherBundle\Entity\Smartcard;
+use VoucherBundle\Entity\SmartcardRedemptionBatch;
 use VoucherBundle\Entity\Vendor;
 use VoucherBundle\Repository\SmartcardPurchaseRepository;
+use VoucherBundle\Repository\SmartcardRedemptionBatchRepository;
 use VoucherBundle\Repository\SmartcardRepository;
 
 class AnalyticsController extends AbstractWebAppController
@@ -185,8 +189,67 @@ class AnalyticsController extends AbstractWebAppController
      *
      * @return JsonResponse
      */
-    public function vendor(Vendor $vendor): JsonResponse
+    public function vendor(
+        Vendor $vendor,
+        SmartcardPurchaseRepository $purchaseRepository
+    ): JsonResponse
     {
-        return $this->json([]);
+        $redemptionBatchRepository = $this->getDoctrine()->getRepository(SmartcardRedemptionBatch::class);
+        $depositSyncRepository = $this->getDoctrine()->getRepository(SynchronizationBatch\Deposits::class);
+        $purchaseSyncRepository = $this->getDoctrine()->getRepository(SynchronizationBatch\Purchases::class);
+
+        $collector = new EventCollector();
+        foreach ($purchaseRepository->findBy(['vendor'=>$vendor]) as $purchase) {
+            $collector->add(new Event('purchase', 'made', $purchase->getCreatedAt(), [
+                'value' => $purchase->getRecordsValue().' '.$purchase->getCurrency(),
+                'assistance_id' => $purchase->getAssistance() ? $purchase->getAssistance()->getId() : null,
+                'assistance_name' => $purchase->getAssistance() ? $purchase->getAssistance()->getName() : null,
+                'purchase_id' => $purchase->getId(),
+                'smartcard_id' => $purchase->getSmartcard()->getId(),
+                'smartcard_serialNumber' => $purchase->getSmartcard()->getSerialNumber(),
+                'vendor_id' => $purchase->getVendor()->getId(),
+                'vendor_name' => $purchase->getVendor()->getName(),
+            ]));
+        }
+        foreach ($redemptionBatchRepository->findByVendor($vendor) as $invoice) {
+            $collector->add(new Event('invoice', 'made', $invoice->getRedeemedAt(), [
+                'value' => $invoice->getValue().' '.$invoice->getCurrency(),
+                'project_id' => $invoice->getProject() ? $invoice->getProject()->getId() : null,
+                'project_name' => $invoice->getProject() ? $invoice->getProject()->getName() : null,
+                'accountant_id' => $invoice->getRedeemedBy()->getId(),
+                'accountant_name' => $invoice->getRedeemedBy()->getUsername(),
+                'vendor_id' => $invoice->getVendor()->getId(),
+                'vendor_name' => $invoice->getVendor()->getName(),
+            ]));
+        }
+        foreach ($depositSyncRepository->findBy(['createdBy' => $vendor]) as $sync) {
+            $collector->add(new Event('deposit sync', 'made', $sync->getCreatedAt(), [
+                'sync_id' => $sync->getId(),
+                'source' => $sync->getSource(),
+                'vendor_id' => $vendor->getId(),
+                'vendor_name' => $vendor->getName(),
+            ]));
+            $collector->add(new Event('deposit sync', 'validated', $sync->getValidatedAt(), [
+                'sync_id' => $sync->getId(),
+                'source' => $sync->getSource(),
+                'vendor_id' => $vendor->getId(),
+                'vendor_name' => $vendor->getName(),
+            ]));
+        }
+        foreach ($purchaseSyncRepository->findBy(['createdBy' => $vendor]) as $sync) {
+            $collector->add(new Event('purchase sync', 'made', $sync->getCreatedAt(), [
+                'sync_id' => $sync->getId(),
+                'source' => $sync->getSource(),
+                'vendor_id' => $vendor->getId(),
+                'vendor_name' => $vendor->getName(),
+            ]));
+            $collector->add(new Event('purchase sync', 'validated', $sync->getValidatedAt(), [
+                'sync_id' => $sync->getId(),
+                'source' => $sync->getSource(),
+                'vendor_id' => $vendor->getId(),
+                'vendor_name' => $vendor->getName(),
+            ]));
+        }
+        return $this->json($collector->getSortedEvents());
     }
 }
