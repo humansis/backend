d1d69e82f4c92f05fda774cfa77e3114f5b52009
Lukas Kral <luke.kral@seznam.cz>
2022-03-03 15:00:04 +0100

    PIN-2872: Add fullname to compare bnf duplicity

--- src/NewApiBundle/Mapper/ImportBeneficiaryDuplicityMapper.php
+++ src/NewApiBundle/Mapper/ImportBeneficiaryDuplicityMapper.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace NewApiBundle\Mapper;
 
 use BeneficiaryBundle\Entity\Beneficiary;
+use NewApiBundle\Component\Import\CellParameters;
 use NewApiBundle\Component\Import\Integrity\ImportLineFactory;
 use NewApiBundle\Component\Import\ValueObject\ImportBeneficiaryDuplicityCompare;
 use NewApiBundle\Entity\ImportBeneficiaryDuplicity;
@@ -58,6 +59,28 @@ class ImportBeneficiaryDuplicityMapper implements MapperInterface
             $this->object->getBeneficiary(),
             $this->object
         );
+    }
+
+    public function getOriginFullName(): string
+    {
+        $person = $this->object->getBeneficiary()->getPerson();
+        if (!empty($person->getLocalFamilyName()) || !empty($person->getLocalGivenName())) {
+            return $person->getLocalFamilyName().' '.$person->getLocalGivenName();
+        } else {
+            return $person->getEnFamilyName().' '.$person->getEnGivenName();
+        }
+    }
+
+    public function getDuplicityFullName(): string
+    {
+        $person = $this->object->getBeneficiary()->getPerson();
+        $userData = $this->object->getQueue()->getContent()[$this->object->getMemberIndex()];
+
+        if (!empty($person->getLocalFamilyName()) || !empty($person->getLocalGivenName())) {
+            return $userData['Local family name'][CellParameters::VALUE].' '.$userData['Local given name'][CellParameters::VALUE];
+        } else {
+            return $userData['English family name'][CellParameters::VALUE].' '.$userData['English given name'][CellParameters::VALUE];
+        }
     }
 
 }
