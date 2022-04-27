fe20966e526f829b13627d0914a016f3671aa0d8
Vit Troi Fux <vit.fux@protonmail.com>
2022-04-27 19:30:53 +0200

    PIN-3249: extend Transaction by ReliefPackage

---
+++ app/DoctrineMigrations/Version20220427172903.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220427172903 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE transaction ADD relief_package_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE transaction ADD CONSTRAINT FK_723705D1370C2938 FOREIGN KEY (relief_package_id) REFERENCES assistance_relief_package (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE transaction DROP FOREIGN KEY FK_723705D1370C2938');
+        $this->addSql('ALTER TABLE transaction DROP relief_package_id');
+    }
+}
