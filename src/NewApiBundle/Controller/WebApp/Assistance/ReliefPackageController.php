6133f24a3939e2b7befb04f6cc84f276cce27992
Vit Troi Fux <vit.fux@protonmail.com>
2022-05-02 15:31:15 +0200

    PIN-3253: added primitive endpoints

---
+++ src/NewApiBundle/Controller/WebApp/Assistance/ReliefPackageController.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace NewApiBundle\Controller\WebApp\Assistance;
+
+use DistributionBundle\Entity\Assistance;
+use NewApiBundle\Controller\AbstractController;
+use NewApiBundle\Entity\Assistance\ReliefPackage;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+class ReliefPackageController extends AbstractController
+{
+    /**
+     * @Rest\Get("/web-app/v1/assistances/{id}/relief-packages")
+     *
+     * @param Assistance $assistance
+     * @param Request    $request
+     *
+     * @return JsonResponse
+     */
+    public function packages(Assistance $assistance, Request $request): JsonResponse
+    {
+        $reliefPackages = $this->getDoctrine()->getRepository(ReliefPackage::class)->findByAssistance($assistance);
+
+        $response = $this->json($reliefPackages);
+        $response->setEtag(md5($response->getContent()));
+        $response->setPublic();
+        $response->isNotModified($request);
+
+        return $response;
+    }
+
+    /**
+     * @Rest\Get("/web-app/v1/assistances/relief-packages/{id}")
+     * @Cache(lastModified="package.getLastModifiedAt()", public=true)
+     *
+     * @param ReliefPackage $package
+     * @param Request       $request
+     *
+     * @return JsonResponse
+     */
+    public function package(ReliefPackage $package, Request $request): JsonResponse
+    {
+        $response = $this->json($package);
+        $response->setEtag(md5($response->getContent()));
+        $response->setPublic();
+        $response->isNotModified($request);
+
+        return $response;
+    }
+}
