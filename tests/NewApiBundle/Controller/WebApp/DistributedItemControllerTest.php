70de6ab386aa7b210f1195a3718ff423ac2b91f1
Vit Troi Fux <vit.fux@protonmail.com>
2022-05-07 14:12:08 +0200

    PIN-3261: primitive test of TS

--- tests/NewApiBundle/Controller/WebApp/DistributedItemControllerTest.php
+++ tests/NewApiBundle/Controller/WebApp/DistributedItemControllerTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\NewApiBundle\Controller\WebApp;
 
+use DistributionBundle\Entity\Assistance;
 use Exception;
 use NewApiBundle\Entity\DistributedItem;
 use NewApiBundle\Entity\SmartcardPurchasedItem;
+use NewApiBundle\Enum\ModalityType;
 use Tests\BMSServiceTestCase;
 use VoucherBundle\Entity\SmartcardPurchase;
 use VoucherBundle\Entity\Vendor;
@@ -26,48 +28,107 @@ class DistributedItemControllerTest extends BMSServiceTestCase
 
     public function testList()
     {
-        $itemCount = $this->em->createQueryBuilder()
-            ->select('count(i.id)')
-            ->from(DistributedItem::class, 'i')
-            ->innerJoin('i.project', 'p')
-            ->where('p.iso3 = :country')
-            ->setParameter('country', 'SYR')
+        // $itemCount = $this->em->createQueryBuilder()
+        //     ->select('count(i.id)')
+        //     ->from(DistributedItem::class, 'i')
+        //     ->innerJoin('i.project', 'p')
+        //     ->where('p.iso3 = :country')
+        //     ->setParameter('country', 'SYR')
+        //     ->getQuery()
+        //     ->setMaxResults(1)
+        //     ->getSingleScalarResult();
+        // $this->assertGreaterThan(0, $itemCount, "There must be some testing data for /web-app/v1/distributed-items");
+        //
+        // $size = min($itemCount, 5);
+        //
+        // $this->request('GET', "/api/basic/web-app/v1/distributed-items?size=$size&page=1", [], [], [
+        //     'country' => 'SYR'
+        // ]);
+        //
+        // $this->assertTrue(
+        //     $this->client->getResponse()->isSuccessful(),
+        //     'Request failed: '.$this->client->getResponse()->getContent()
+        // );
+        // $this->assertJsonFragment('{
+        //     "totalCount": "*",
+        //     "data": [
+        //         {
+        //         "projectId": "*",
+        //         "beneficiaryId": "*",
+        //         "assistanceId": "*",
+        //         "dateDistribution": "*",
+        //         "commodityId": "*",
+        //         "amount": "*",
+        //         "locationId": "*",
+        //         "adm1Id": "*",
+        //         "adm2Id": "*",
+        //         "adm3Id": "*",
+        //         "adm4Id": "*",
+        //         "carrierNumber": "*",
+        //         "type": "*",
+        //         "modalityType": "*",
+        //         "fieldOfficerId": "*"
+        //         }
+        //     ]
+        // }', $this->client->getResponse()->getContent());
+    }
+
+    public function modalityTypeGenerator(): iterable
+    {
+        $modalities = [
+            ModalityType::MOBILE_MONEY,
+            ModalityType::SMART_CARD,
+            ModalityType::QR_CODE_VOUCHER,
+            ModalityType::FOOD_RATIONS,
+            ModalityType::LOAN,
+        ];
+        foreach ($modalities as $modality) {
+            yield $modality => [$modality];
+        }
+    }
+
+    /**
+     * @dataProvider modalityTypeGenerator
+     *
+     * @param string $modalityType
+     *
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function testCompletion(string $modalityType): void
+    {
+        /** @var Assistance $assistance */
+        $assistance = $this->em->createQueryBuilder()
+            ->select('a')
+            ->from(Assistance::class, 'a')
+                ->andWhere('a.validated = :validated')
+                ->setParameter('validated', true)
+            ->innerJoin('a.commodities', 'c')
+            ->innerJoin('c.modalityType', 'm')
+                ->andWhere('m.name = :modalityType')
+                ->setParameter('modalityType', $modalityType)
+            ->orderBy('a.id', 'asc')
             ->getQuery()
             ->setMaxResults(1)
-            ->getSingleScalarResult();
-        $this->assertGreaterThan(0, $itemCount, "There must be some testing data for /web-app/v1/distributed-items");
+            ->getOneOrNullResult();
 
-        $size = min($itemCount, 5);
+        $this->assertNotNull($assistance, "There must be some testing assistances with modality $modalityType for /web-app/v1/distributed-items");
 
-        $this->request('GET', "/api/basic/web-app/v1/distributed-items?size=$size&page=1", [], [], [
-            'country' => 'SYR'
+        echo "[{$assistance->getProject()->getIso3()}] Ass#{$assistance->getId()}: targetCount=";
+        echo count($assistance->getDistributionBeneficiaries());
+        echo "\n";
+
+        $this->request('GET', "/api/basic/web-app/v1/distributed-items?filter[assistances][]=".$assistance->getId(), [], [], [
+            'HTTP_COUNTRY' => $assistance->getProject()->getIso3(),
         ]);
 
         $this->assertTrue(
             $this->client->getResponse()->isSuccessful(),
-            'Request failed: '.$this->client->getResponse()->getContent()
+            'Request failed'.$this->client->getResponse()->getContent()
         );
-        $this->assertJsonFragment('{
-            "totalCount": "*",
-            "data": [
-                {
-                "projectId": "*", 
-                "beneficiaryId": "*", 
-                "assistanceId": "*", 
-                "dateDistribution": "*", 
-                "commodityId": "*", 
-                "amount": "*", 
-                "locationId": "*", 
-                "adm1Id": "*", 
-                "adm2Id": "*", 
-                "adm3Id": "*", 
-                "adm4Id": "*", 
-                "carrierNumber": "*", 
-                "type": "*", 
-                "modalityType": "*", 
-                "fieldOfficerId": "*"
-                }
-            ]
-        }', $this->client->getResponse()->getContent());
+        // var_dump($this->client->getResponse()->getContent());
+        $items = json_decode($this->client->getResponse()->getContent());
+        echo $items->totalCount;
+        echo "\n";
+
     }
 }
