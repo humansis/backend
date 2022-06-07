humansis/api

REFACTORING: removed inconsistent and duplicity link between AbstractBeneficiary and DistributionBeneficiary

src/BeneficiaryBundle/Repository/BeneficiaryRepository.php
@@ -129,7 +129,7 @@ class BeneficiaryRepository extends AbstractCriteriaRepository
     public function getAllofDistribution(Assistance $assistance)
     {
         $qb = $this->createQueryBuilder('b');
-        $q = $qb->leftJoin('b.distributionBeneficiary', 'db')
+        $q = $qb->leftJoin('b.distributionBeneficiaries', 'db')
             ->where('db.assistance = :assistance')
             ->setParameter('assistance', $assistance);
 
@@ -139,7 +139,7 @@ class BeneficiaryRepository extends AbstractCriteriaRepository
     public function getNotRemovedofDistribution(Assistance $assistance)
     {
         $qb = $this->createQueryBuilder('b');
-        $q = $qb->leftJoin('b.distributionBeneficiary', 'db')
+        $q = $qb->leftJoin('b.distributionBeneficiaries', 'db')
             ->where('db.assistance = :assistance')
             ->andWhere('db.removed = 0')
             ->setParameter('assistance', $assistance);
@@ -404,7 +404,7 @@ class BeneficiaryRepository extends AbstractCriteriaRepository
         $this->beneficiariesInCountry($qb, $iso3);
 
         $qb->select('COUNT(DISTINCT b)')
-            ->leftJoin('b.distributionBeneficiary', 'db')
+            ->leftJoin('b.distributionBeneficiaries', 'db')
             ->leftJoin('db.booklets', 'bk')
             ->leftJoin('db.transactions', 't')
             ->leftJoin('db.generalReliefs', 'gri')

humansis/backend

src/BeneficiaryBundle/Repository/BeneficiaryRepository.php
@@ -294,7 +294,7 @@ class BeneficiaryRepository extends AbstractCriteriaRepository
     public function getAllofDistribution(Assistance $assistance)
     {
         $qb = $this->createQueryBuilder('b');
-        $q = $qb->leftJoin('b.assistanceBeneficiary', 'db')
+        $q = $qb->leftJoin('b.assistanceBeneficiaries', 'db')
             ->where('db.assistance = :assistance')
             ->setParameter('assistance', $assistance);
 
@@ -314,7 +314,7 @@ class BeneficiaryRepository extends AbstractCriteriaRepository
     public function getNotRemovedofDistribution(Assistance $assistance)
     {
         $qb = $this->createQueryBuilder('b');
-        $q = $qb->leftJoin('b.assistanceBeneficiary', 'db')
+        $q = $qb->leftJoin('b.assistanceBeneficiaries', 'db')
             ->where('db.assistance = :assistance')
             ->andWhere('db.removed = 0')
             ->setParameter('assistance', $assistance);
@@ -580,7 +580,7 @@ class BeneficiaryRepository extends AbstractCriteriaRepository
         $this->beneficiariesInCountry($qb, $iso3);
 
         $qb->select('COUNT(DISTINCT b)')
-            ->leftJoin('b.assistanceBeneficiary', 'db')
+            ->leftJoin('b.assistanceBeneficiaries', 'db')
             ->leftJoin('db.booklets', 'bk')
             ->leftJoin('db.transactions', 't')
             ->leftJoin('db.reliefPackages', 'rp')