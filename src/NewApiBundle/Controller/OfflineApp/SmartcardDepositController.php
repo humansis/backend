4db71b8d71a86e08756b400d75a555591f88afea
Lukas Kral <luke.kral@seznam.cz>
2022-06-07 14:40:34 +0200

    [PIN-3447]: Autowire Kernel

--- src/NewApiBundle/Controller/OfflineApp/SmartcardDepositController.php
+++ src/NewApiBundle/Controller/OfflineApp/SmartcardDepositController.php
@@ -19,18 +19,18 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\HttpKernel\KernelInterface;
 use VoucherBundle\Entity\SmartcardDeposit;
 use VoucherBundle\Repository\SmartcardDepositRepository;
 
 class SmartcardDepositController extends AbstractOfflineAppController
 {
     /**
-     * @var Kernel
+     * @var KernelInterface
      */
     private $kernel;
 
-    public function __construct(Kernel $kernel)
+    public function __construct(KernelInterface $kernel)
     {
         $this->kernel = $kernel;
     }
