b8813ab0833affdd1f07399a4b09052a41036697
Vit Troi Fux <vit.fux@protonmail.com>
2022-06-14 17:46:02 +0200

    [PIN-3504] removed unused methods

--- src/NewApiBundle/Component/Assistance/SelectionCriteriaFactory.php
+++ src/NewApiBundle/Component/Assistance/SelectionCriteriaFactory.php
@@ -108,68 +108,6 @@ class SelectionCriteriaFactory
         return $criterium;
     }
 
-    /**
-     * @param string $condition
-     * @param string $field
-     * @param string $target
-     * @param $valueString
-     * @param int    $weight
-     *
-     * @return SelectionCriteriaEntity
-     */
-    public function createPersonnal(
-        string $condition,
-        string $field,
-        string $target,
-        $valueString,
-        int $weight
-    ): SelectionCriteriaEntity
-    {
-        $criteria = new SelectionCriteriaEntity();
-        $criteria->setConditionString($condition);
-        $criteria->setFieldString($field);
-        $criteria->setTarget($target);
-        $criteria->setValueString($valueString);
-        $criteria->setWeight($weight);
-        $criteria->setTableString('Personnal');
-        return $criteria;
-    }
-
-    public function createCountrySpecific(
-        string $condition,
-        string $field,
-        string $target,
-               $valueString,
-        int $weight
-    ): SelectionCriteriaEntity
-    {
-        $criteria = new SelectionCriteriaEntity();
-        $criteria->setConditionString($condition);
-        $criteria->setFieldString($field);
-        $criteria->setTarget($target);
-        $criteria->setValueString($valueString);
-        $criteria->setWeight($weight);
-        $criteria->setTableString('countrySpecific');
-        return $criteria;
-    }
-
-    public function createVulnerability(
-        string $field,
-        string $target,
-               $valueString,
-        int $weight
-    ): SelectionCriteriaEntity
-    {
-        $criteria = new SelectionCriteriaEntity();
-        $criteria->setConditionString($valueString);
-        $criteria->setFieldString($field);
-        $criteria->setTarget($target);
-        $criteria->setValueString(null);
-        $criteria->setWeight($weight);
-        $criteria->setTableString('vulnerabilityCriteria');
-        return $criteria;
-    }
-
     public function hydrate(SelectionCriteriaEntity $criteriaEntity): SelectionCriteria
     {
         return new SelectionCriteria(
