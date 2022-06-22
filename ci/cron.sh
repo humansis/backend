19a450399a462896b904fa4d9adfbe2c4761c341
Vit Troi Fux <vit.fux@protonmail.com>
2022-06-22 15:49:41 +0200

    remove cron finishing

--- ci/cron.sh
+++ ci/cron.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd /opt/humansis
-docker-compose exec -T php bash -c 'php bin/console app:import:finish' || true
-
 docker-compose exec -T php bash -c 'php bin/console app:import:load' || true
 
 docker-compose exec -T php bash -c 'php bin/console app:import:clean' || true
