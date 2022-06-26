1655953333991b9e25aa4d78c0ecfa5f1308595a
Pavlix <pavel.kraus01@gmail.com>
2022-06-26 14:27:53 +0200

    [PIN-3438] Added unique index to country specific answer of household

---
+++ app/DoctrineMigrations/Version20220626120802.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220626120802 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE UNIQUE INDEX only_one_household_answer ON country_specific_answer (country_specific_id, household_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+
+    }
+}
