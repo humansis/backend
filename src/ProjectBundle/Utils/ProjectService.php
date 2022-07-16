406e024fdeaab01da636278030df413f1cd11770
Pavlix <pavel.kraus01@gmail.com>
2022-07-16 18:48:09 +0200

    [PIN-2708] ProjectBundle - removed unused methods in services

--- src/ProjectBundle/Utils/ProjectService.php
+++ src/ProjectBundle/Utils/ProjectService.php
@@ -3,7 +3,6 @@
 namespace ProjectBundle\Utils;
 
 use NewApiBundle\Entity\Household;
-use dateTime;
 use DistributionBundle\Entity\Assistance;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityNotFoundException;
@@ -13,15 +12,10 @@ use NewApiBundle\Exception\ConstraintViolationException;
 use NewApiBundle\InputType\AddHouseholdsToProjectInputType;
 use NewApiBundle\InputType\ProjectCreateInputType;
 use NewApiBundle\InputType\ProjectUpdateInputType;
-use Symfony\Component\Serializer\SerializerInterface as Serializer;
 use ProjectBundle\Entity\Donor;
 use ProjectBundle\Entity\Project;
-use ProjectBundle\DTO\Sector;
 use Psr\Container\ContainerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Validator\ConstraintViolation;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use UserBundle\Entity\User;
 use UserBundle\Entity\UserProject;
 
@@ -36,59 +30,18 @@ class ProjectService
      */
     protected $em;
 
-    /** @var Serializer $serializer */
-    private $serializer;
-
-    /** @var ValidatorInterface $validator */
-    private $validator;
-
     /** @var ContainerInterface $container */
     private $container;
 
     /**
      * ProjectService constructor.
      * @param EntityManagerInterface $entityManager
-     * @param Serializer $serializer
-     * @param ValidatorInterface $validator
      * @param ContainerInterface $container
      */
-    public function __construct(EntityManagerInterface $entityManager, Serializer $serializer, ValidatorInterface $validator, ContainerInterface $container)
+    public function __construct(EntityManagerInterface $entityManager, ContainerInterface $container)
     {
         $this->em = $entityManager;
-        $this->serializer = $serializer;
-        $this->validator = $validator;
         $this->container = $container;
-    }
-
-    /**
-     * Get all projects
-     *
-     * @param $countryIso3
-     * @param User $user
-     * @return array
-     */
-    public function findAll($countryIso3, User $user)
-    {
-        if (
-            !$user->hasRole("ROLE_COUNTRY_MANAGER")
-            && !$user->hasRole("ROLE_REGIONAL_MANAGER")
-            && !$user->hasRole("ROLE_ADMIN")
-        ) {
-            $projects = $this->em->getRepository(Project::class)->getAllOfUser($user);
-        } else {
-            $projects = $this->em->getRepository(Project::class)->getAllOfCountry($countryIso3);
-        }
-        return $projects;
-    }
-
-    /**
-     * @param string $iso3
-     * @return mixed
-     */
-    public function countAll(string $iso3)
-    {
-        $count = $this->em->getRepository(Project::class)->count(['iso3' => $iso3, 'archived' => 0]);
-        return $count;
     }
 
     /**
@@ -99,88 +52,6 @@ class ProjectService
     {
         $count = $this->em->getRepository(Project::class)->countActiveInCountry($iso3);
         return $count;
-    }
-
-    /**
-     * Create a project
-     *
-     * @param $countryISO3
-     * @param array $projectArray
-     * @param User $user
-     * @return Project
-     * @throws \Exception
-     */
-    public function createFromArray($countryISO3, array $projectArray, User $user)
-    {
-        /** @var Project $project */
-
-        $startDate = DateTime::createFromFormat('d-m-Y', $projectArray["start_date"]);
-        $endDate = DateTime::createFromFormat('d-m-Y', $projectArray["end_date"]);
-
-        if ($startDate > $endDate) {
-            throw new \Exception('The end date must be after the start date', Response::HTTP_BAD_REQUEST);
-        }
-
-        $project = new Project();
-        $project->setName($projectArray["name"])
-                ->setStartDate($startDate)
-                ->setEndDate($endDate)
-                ->setIso3($countryISO3)
-                ->setTarget($projectArray["target"])
-                ->setNotes($projectArray["notes"]);
-
-        if (isset($projectArray["internal_id"])) {
-            $project->setInternalId($projectArray["internal_id"]);
-        }
-
-        $existingProject = $this->em->getRepository(Project::class)->findBy(
-            [
-                'name' => $project->getName(),
-                'iso3' => $project->getIso3(),
-            ]
-        );
-        if (!empty($existingProject)) {
-            throw new HttpException(Response::HTTP_CONFLICT, 'Project with the name '.$project->getName().' already exists');
-        }
-
-        $errors = $this->validator->validate($project);
-        if (count($errors) > 0) {
-            $errorsArray = [];
-            foreach ($errors as $error) {
-                $errorsArray[] = $error->getMessage();
-            }
-            throw new \Exception(json_encode($errorsArray), Response::HTTP_BAD_REQUEST);
-        }
-
-        $sectorsId = $projectArray["sectors"];
-        if (count($sectorsId) > 0) {
-            $project->getSectors()->clear();
-            /** @var Sector $sector */
-            foreach ($sectorsId as $sectorId) {
-                $project->addSector($sectorId);
-            }
-        } else {
-            throw new HttpException(Response::HTTP_BAD_REQUEST, 'Project must have at least one sector');
-        }
-
-        $donorsId = $projectArray["donors"];
-        if (null !== $donorsId) {
-            $project->getDonors()->clear();
-            /** @var Donor $donor */
-            foreach ($donorsId as $donorId) {
-                $donorTmp = $this->em->getRepository(Donor::class)->find($donorId);
-                if ($donorTmp instanceof Donor) {
-                    $project->addDonor($donorTmp);
-                }
-            }
-        }
-
-        $this->em->persist($project);
-        $this->em->flush();
-
-        $this->addUser($project, $user);
-
-        return $project;
     }
 
     /**
@@ -235,71 +106,6 @@ class ProjectService
     }
 
     /**
-     * @param Project $project
-     * @param array $projectArray
-     * @return array|bool|Project
-     * @throws \Exception
-     */
-    public function edit(Project $project, array $projectArray)
-    {
-        $startDate = DateTime::createFromFormat('d-m-Y', $projectArray["start_date"]);
-        $endDate = DateTime::createFromFormat('d-m-Y', $projectArray["end_date"]);
-
-        if ($startDate > $endDate) {
-            throw new \Exception('The end date must be after the start date', Response::HTTP_BAD_REQUEST);
-        }
-    
-        /** @var Project $editedProject */
-        $oldProject = $this->em->getRepository(Project::class)->find($project->getId());
-        if ($oldProject->getArchived() == 0) {
-            $project->setName($projectArray['name'])
-                ->setStartDate($startDate)
-                ->setEndDate($endDate)
-                ->setTarget($projectArray['target'])
-                ->setNotes($projectArray["notes"]);
-
-            if (isset($projectArray["internal_id"])) {
-                $project->setInternalId($projectArray["internal_id"]);
-            }
-
-            $project->setSectors($projectArray['sectors'] ?? []);
-
-            $donors = $projectArray['donors'];
-
-            if (null !== $donors) {
-                $project->removeDonors();
-                /** @var Donor $donor */
-                foreach ($donors as $donor) {
-                    $newDonor = $this->em->getRepository(Donor::class)->find($donor);
-                    if ($newDonor instanceof Donor) {
-                        $project->addDonor($newDonor);
-                    }
-                }
-            }
-
-            $errors = $this->validator->validate($project);
-            if (count($errors) > 0) {
-                $errorsArray = [];
-                foreach ($errors as $error) {
-                    $errorsArray[] = $error->getMessage();
-                }
-                throw new \Exception(json_encode($errorsArray), Response::HTTP_BAD_REQUEST);
-            }
-
-            $this->em->persist($project);
-            try {
-                $this->em->flush();
-            } catch (\Exception $e) {
-                return false;
-            }
-
-            return $project;
-        } else {
-            return ['error' => 'The project is archived'];
-        }
-    }
-
-    /**
      * @param Project                $project
      * @param ProjectUpdateInputType $inputType
      *
@@ -346,34 +152,6 @@ class ProjectService
         $this->em->flush();
 
         return $project;
-    }
-
-    /**
-     * @deprecated remove in 3.0
-     *
-     * Add multiple households to project.
-     *
-     * @param Project $project
-     * @param $households
-     * @return array
-     */
-    public function addMultipleHouseholds(Project $project, $households)
-    {
-        foreach ($households as $hh) {
-            if (!$hh instanceof Household) {
-                $hh = $this->em->getRepository(Household::class)->find($hh['id']);
-            }
-
-            $projectHousehold = $hh->getProjects()->contains($project);
-            if (!$projectHousehold) {
-                $hh->addProject($project);
-            }
-            $this->em->persist($hh);
-        }
-        $this->em->persist($project);
-        $this->em->flush();
-
-        return $households ;
     }
 
     /**
