33eef1103cdc17a27b5ca13f7f8051983af18467
Pavlix <pavel.kraus01@gmail.com>
2022-07-17 14:01:56 +0200

    [PIN-2708] ReportingBundle - removed services and commands

--- docker/scripts/clean.sh
+++ docker/scripts/clean.sh
@@ -8,7 +8,6 @@ php bin/console d:d:c
 php bin/console d:m:m -n
 php bin/console d:f:l -n
 php bin/console r:c:c
-php bin/console reporting:code-indicator:add
 rm -rf var/cache/*
 php bin/console cache:clear
 
