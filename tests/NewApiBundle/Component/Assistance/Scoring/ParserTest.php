6e3745d146cd603cd9c9b4390dae3d646287133e
Ondrej Kalendovsky <ondrej.kalendovsky@quanti.cz>
2022-07-27 15:16:02 +0200

    PIN-3629 BE: Update SYR scoring - fixes - test enums (rule type)

--- tests/NewApiBundle/Component/Assistance/Scoring/ParserTest.php
+++ tests/NewApiBundle/Component/Assistance/Scoring/ParserTest.php
@@ -38,6 +38,17 @@ class ParserTest extends KernelTestCase
         $this->assertEquals(1, count($scoringRules[0]->getOptions()));
         $this->assertEquals(1, count($scoringRules[1]->getOptions()));
         $this->assertEquals(2, count($scoringRules[2]->getOptions()));
+        $this->assertEquals(2, count($scoringRules[3]->getOptions()));
+        $this->assertEquals(7, count($scoringRules[4]->getOptions()));
+        $this->assertEquals(7, count($scoringRules[5]->getOptions()));
+        $this->assertEquals(6, count($scoringRules[6]->getOptions()));
+        $this->assertEquals(5, count($scoringRules[7]->getOptions()));
+        $this->assertEquals(4, count($scoringRules[8]->getOptions()));
+        $this->assertEquals(4, count($scoringRules[9]->getOptions()));
+        $this->assertEquals(3, count($scoringRules[10]->getOptions()));
+        $this->assertEquals(2, count($scoringRules[11]->getOptions()));
+        $this->assertEquals(5, count($scoringRules[12]->getOptions()));
+        $this->assertEquals(10, count($scoringRules[13]->getOptions()));
 
         foreach ($scoringRules as $rule) {
             $this->assertContains($rule->getFieldName(), ScoringRulesCalculationsEnum::values());
@@ -51,6 +62,16 @@ class ParserTest extends KernelTestCase
     public function testParseCountrySpecific()
     {
         $csvPath = $this->projectDir .'/tests/NewApiBundle/Resources/Scoring/single_country_specific.csv';
+
+        /** @var ScoringRule[] $scoringRules */
+        $scoringRules = $this->parser->parse($csvPath);
+
+        $this->assertEquals(1, count($scoringRules));
+    }
+
+    public function testParseAllSupportedEnums()
+    {
+        $csvPath = $this->projectDir .'/tests/NewApiBundle/Resources/Scoring/all_supported_enums.csv';
 
         /** @var ScoringRule[] $scoringRules */
         $scoringRules = $this->parser->parse($csvPath);
