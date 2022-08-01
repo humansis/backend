e1f5567236e7542c3ff627f880148894359316e6
David Slacalek <david.slacalek@quanti.cz>
2022-08-01 16:16:08 +0200

    [PIN-3696] remove old answers for households where multiple answers detected

--- app/DoctrineMigrations/Version20220626120802.php
+++ app/DoctrineMigrations/Version20220626120802.php
@@ -15,6 +15,9 @@ final class Version20220626120802 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        //remove old answers for households where multiple answers detected - PIN-3696
+        $this->addSql('DELETE FROM country_specific_answer WHERE id IN (25962,25963,25966,25967,25968,25969,25970,25971,25972,25973,25974,25975,25976,25977,25978,25979,25980,25981,25982,25983,25984,25985,25986,25987,25988,25989,25990,25991,25992,25993,25994,25995,25996,25997,25998,25999,26000,26133,26134,26135,26136,26137,26138,26139,26140,26141,26142,26143,26144,26145,26146,26147,26148,26149,26150,26151,26152,26153,26154,26155,26156,26157,26158,26159,26160,26161,26162,26163,26164,26165,26166,26167,26168,26169,26170,26171,26172,26173,26174,26175,26176,26177,26178)');
+
         $this->addSql('CREATE UNIQUE INDEX only_one_household_answer ON country_specific_answer (country_specific_id, household_id)');
     }
 
