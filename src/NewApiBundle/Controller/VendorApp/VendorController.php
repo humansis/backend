59ff06fa2985b47afb1aeaed4b55d0ec94ae622a
Pavlix <pavel.kraus01@gmail.com>
2022-08-15 13:52:59 +0200

    [PIN-2708] VoucherBundle: removed VendorController

---
+++ src/NewApiBundle/Controller/VendorApp/VendorController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace NewApiBundle\Controller\VendorApp;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Response;
+use VoucherBundle\Entity\Vendor;
+
+class VendorController extends Controller
+{
+    /**
+     * Get single vendor.
+     *
+     * @Rest\Get("/vendor-app/v1/vendors/{id}")
+     *
+     * @param Vendor $vendor
+     *
+     * @return Response
+     */
+    public function getSingleActionVendor(Vendor $vendor)
+    {
+        $json = $this->get('serializer')->serialize($vendor, 'json', ['groups' => ['FullVendor']]);
+
+        return new Response($json);
+    }
+}
