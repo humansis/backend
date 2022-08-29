80d6a4ff75a05551199947dc134b5d3ae4419415
Vladimír Blažek <vladimir.blazek@quanti.cz>
2022-08-29 15:41:48 +0200

    [PIN-3830] Fix compatibility issues

--- tests/BMSServiceTestCase.php
+++ tests/BMSServiceTestCase.php
@@ -284,7 +284,7 @@ class BMSServiceTestCase extends KernelTestCase
     /**
      * {@inheritDoc}
      */
-    protected function tearDown()
+    protected function tearDown(): void
     {
         //parent::tearDown();
         if (!empty($this->em)) {
