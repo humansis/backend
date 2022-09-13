3da2260f2bb268eef345bb1d55be0a8de0523335
David Slacalek <david.slacalek@quanti.cz>
2022-09-13 15:32:01 +0200

    [PIN-3486] extract request to trait

--- src/NewApiBundle/Command/Crowdin/CrowdinPushCommand.php
+++ src/NewApiBundle/Command/Crowdin/CrowdinPushCommand.php
@@ -21,6 +21,8 @@ use UnexpectedValueException;
  */
 class CrowdinPushCommand extends Command
 {
+    use CrowdinRequestTrait;
+    
     /** @var HttpClient $client */
     private $client;
 
@@ -137,35 +139,5 @@ class CrowdinPushCommand extends Command
         }
 
         return 0;
-    }
-
-    /**
-     * @throws TransportExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws JsonException
-     */
-    private function makeRequest(string $method, string $endpoint, array $options = []): array
-    {
-        $url = 'https://api.crowdin.com/api/v2'.$endpoint;
-
-        $options['headers']['Authorization'] = 'Bearer '.$this->crowdinApiKey;
-        if (!isset($options['headers']['Content-Type'])) {
-            $options['headers']['Content-Type'] = 'application/json';
-        }
-
-        $response = $this->client->request($method, $url, $options);
-        $statusCode = $response->getStatusCode();
-        $content = $response->getContent(false);
-        $headers = $response->getHeaders(false);
-
-        if ($statusCode >= 400) {
-            $this->output->writeln('<error>'.$method.' '.$url."\n".$statusCode.': '.$content.'</error>');
-            throw new \RuntimeException('Request failed with status code '.$statusCode);
-        }
-
-        return array_merge(
-            ['headers' => $headers],
-            json_decode($content, true, 512, JSON_THROW_ON_ERROR)
-        );
     }
 }
