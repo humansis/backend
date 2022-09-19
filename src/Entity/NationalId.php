7bbf20cf474070e1b6b1e16dce9fffcb06527037
Vladimír Blažek <vladimir.blazek@quanti.cz>
2022-09-19 16:11:03 +0200

    Fix

--- src/Entity/NationalId.php
+++ src/Entity/NationalId.php
@@ -40,7 +40,6 @@ class NationalId
      * @var int
      *
      * @ORM\Column(name="priority", type="integer")
-     * @SymfonyGroups({"FullHousehold", "SmallHousehold", "FullReceivers", "FullInstitution"})
      */
     private $priority;
 
