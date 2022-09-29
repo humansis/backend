e8540ce934a93be06727a4367e28557855a57caa
Abdulrahman Alamouri <alaabd01@pinf.cz>
2022-09-29 12:13:13 +0300

    Create an interface for export and apply it to export - Beneficiaries

---
+++ src/Utils/ExportTableServiceInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Utils;
+
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+/**
+ * Interface ExportTableServiceInterface
+ *
+ * @package Utils
+ */
+interface ExportTableServiceInterface
+{
+
+    /**
+     * return spreadsheet as StreamedResponse
+     *
+     * @param        $exportableTable
+     * @param string $name
+     * @param string $format
+     * @param bool   $headerDown
+     * @param bool   $headerBold
+     * @param bool   $headerFontItalic
+     *
+     * @return StreamedResponse
+     */
+    public function export(
+        $exportableTable,
+        string $name,
+        string $format,
+        bool $headerDown = false,
+        bool $headerBold = false,
+        bool $headerFontItalic = false): StreamedResponse;
+}
