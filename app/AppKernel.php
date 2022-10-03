5bbcb77b9fbe685198330563b4f24bfb5a18f3de
Lukas Kral <luke.kral@seznam.cz>
2022-10-03 11:21:17 +0200

    [PIN-3648]: DH Auditor

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -6,13 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\Routing\RouteCollectionBuilder;
 
 class AppKernel extends Kernel
 {
+
     use MicroKernelTrait;
 
-    public const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+    const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+
 
     public function getRootDir()
     {
@@ -21,12 +22,12 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        return dirname(__DIR__) . '/var/logs';
+        return dirname(__DIR__).'/var/logs';
     }
 
     public function registerBundles()
@@ -48,7 +49,9 @@ class AppKernel extends Kernel
             new Jrk\LevenshteinBundle\JrkLevenshteinBundle(),
             new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
             new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
+            new \DH\AuditorBundle\DHAuditorBundle()
         ];
+
 
         if (in_array($this->getEnvironment(), ['local', 'dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
@@ -63,37 +66,38 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
+
     protected function configureContainer(ContainerBuilder $containerBuilder, LoaderInterface $loader)
     {
-        $confDir = $this->getProjectDir() . '/app/config';
-        $loader->load($confDir . '/config' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/packages/*' . self::CONFIG_EXTS, 'glob');
-        if (is_dir($confDir . '/packages/' . $this->environment)) {
-            $loader->load($confDir . '/packages/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+        $confDir = $this->getProjectDir().'/app/config';
+        $loader->load($confDir.'/config'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/dh_auditor'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/packages/*'.self::CONFIG_EXTS, 'glob');
+        if (is_dir($confDir.'/packages/'.$this->environment)) {
+            $loader->load($confDir.'/packages/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
         }
-        $loader->load($confDir . '/services' . self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir . '/services_' . $this->environment . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/services'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/services_'.$this->environment.self::CONFIG_EXTS, 'glob');
 
         $containerBuilder->addCompilerPass(new MapperCompilerPass());
 
         $mappings = [
-            realpath(__DIR__ . '/../src/Resources/config/doctrine/model') => 'FOS\UserBundle\Model',
+            realpath(__DIR__.'/../src/Resources/config/doctrine/model') => 'FOS\UserBundle\Model',
         ];
 
         //Compiler pass added for overriding user-bundle User entity mapping of roles.
         $containerBuilder->addCompilerPass(
             DoctrineOrmMappingsPass::createXmlMappingDriver(
-                $mappings,
-                ['fos_user.model_manager_name'],
-                false
+                $mappings, ['fos_user.model_manager_name'], false
             )
         );
     }
 
-    protected function configureRoutes(RouteCollectionBuilder $routes)
+
+    protected function configureRoutes(\Symfony\Component\Routing\RouteCollectionBuilder $routes)
     {
-        $routes->import($this->getRootDir() . '/config/routing.yml');
-        $environmentRoutingConfig = $this->getRootDir() . '/config/routing/' . $this->getEnvironment() . '/routing.yml';
+        $routes->import($this->getRootDir().'/config/routing.yml');
+        $environmentRoutingConfig = $this->getRootDir().'/config/routing/'.$this->getEnvironment().'/routing.yml';
         if (is_file($environmentRoutingConfig)) {
             $routes->import($environmentRoutingConfig);
         }
