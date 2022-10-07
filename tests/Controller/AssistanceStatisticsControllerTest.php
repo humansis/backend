304e3870dc59ac8e5d560bcfd6ac50508aedf0ed
David Slacalek <david.slacalek@quanti.cz>
2022-10-07 16:12:31 +0200

    [PIN-4026] codestyle cleanup

--- tests/Controller/AssistanceStatisticsControllerTest.php
+++ tests/Controller/AssistanceStatisticsControllerTest.php
@@ -64,8 +64,9 @@ class AssistanceStatisticsControllerTest extends BMSServiceTestCase
             $this->client->getResponse()->isSuccessful(),
             'Request failed: ' . $this->client->getResponse()->getContent()
         );
-        $this->assertJsonFragment('{
-            "id": '.$assistance->getId().',
+        $this->assertJsonFragment(
+            '{
+            "id": ' . $assistance->getId() . ',
             "beneficiariesTotal": "*",
             "amountTotal": "*",
             "amountDistributed": "*",
@@ -73,7 +74,9 @@ class AssistanceStatisticsControllerTest extends BMSServiceTestCase
             "amountSent": "*",
             "amountPickedUp": "*",
             "beneficiariesDeleted": "*"
-        }', $this->client->getResponse()->getContent());
+        }',
+            $this->client->getResponse()->getContent()
+        );
     }
 
     public function testList()
