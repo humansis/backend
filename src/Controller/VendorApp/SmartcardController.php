23d19b8f500cb04e6c547dc782d87fe3f57c9b27
Abdulrahman Alamouri <alaabd01@pinf.cz>
2022-10-24 11:35:39 +0000

    [PIN-4149] Fix DI - VendorApp

--- src/Controller/VendorApp/SmartcardController.php
+++ src/Controller/VendorApp/SmartcardController.php
@@ -7,13 +7,46 @@ namespace Controller\VendorApp;
 use Exception;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use InputType\SmartcardPurchaseInputType;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Entity\Smartcard;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Utils\SmartcardService;
 
 class SmartcardController extends AbstractVendorAppController
 {
+    /** @var SerializerInterface */
+    private $serializer;
+
+    /** @var ValidatorInterface */
+    private $validator;
+
+    /** @var LoggerInterface */
+    private $logger;
+
+    /** @var SmartcardService */
+    private $smartcardService;
+
+    /** @var string */
+    private $logsDir;
+
+    public function __construct(
+        SerializerInterface $serializer,
+        ValidatorInterface $validator,
+        LoggerInterface $logger,
+        SmartcardService $smartcardService,
+        string $logsDir
+    ) {
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+        $this->logger = $logger;
+        $this->smartcardService = $smartcardService;
+        $this->logsDir = $logsDir;
+    }
+    /** @var  */
     /**
      * //TODO whole endpoint should be removed after syncs of purchases
      *
@@ -27,17 +60,17 @@ class SmartcardController extends AbstractVendorAppController
     public function beneficiaries(Request $request): Response
     {
         /** @var SmartcardPurchaseInputType $data */
-        $data = $this->get('serializer')->deserialize(
+        $data = $this->serializer->deserialize(
             $request->getContent(),
             SmartcardPurchaseInputType::class,
             'json'
         );
 
-        $errors = $this->get('validator')->validate($data);
+        $errors = $this->validator->validate($data);
 
         //TODO remove after syncs for purchases will be implemented
         if (count($errors) > 0) {
-            $this->container->get('logger')->error(
+            $this->logger->error(
                 'validation errors: ' . ((string) $errors) . ' data: ' . json_encode($request->request->all())
             );
 
@@ -52,7 +85,7 @@ class SmartcardController extends AbstractVendorAppController
         }
 
         try {
-            $purchase = $this->get('smartcard_service')->purchase($request->get('serialNumber'), $data);
+            $purchase = $this->smartcardService->purchase($request->get('serialNumber'), $data);
         } catch (Exception $exception) {
             $this->writeData(
                 'purchaseV3',
@@ -63,7 +96,7 @@ class SmartcardController extends AbstractVendorAppController
             throw $exception;
         }
 
-        $json = $this->get('serializer')->serialize(
+        $json = $this->serializer->serialize(
             $purchase->getSmartcard(),
             'json',
             ['groups' => ['SmartcardOverview']]
@@ -74,7 +107,7 @@ class SmartcardController extends AbstractVendorAppController
 
     private function writeData(string $type, string $user, string $smartcard, $data): void
     {
-        $filename = $this->get('kernel')->getLogDir() . '/';
+        $filename = $this->logsDir . '/';
         $filename .= implode('_', ['SC-invalidData', $type, 'vendor-' . $user, 'sc-' . $smartcard . '.json']);
         $logFile = fopen($filename, "a+");
         fwrite($logFile, $data);
--- src/Controller/VendorApp/VoucherController.php
+++ src/Controller/VendorApp/VoucherController.php
@@ -4,16 +4,46 @@ namespace Controller\VendorApp;
 
 use Doctrine\ORM\EntityNotFoundException;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Model\PurchaseService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Constraints\All;
 use Symfony\Component\Validator\Constraints\Type;
 use Symfony\Component\Validator\Constraints\Valid;
 use InputType\VoucherPurchase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Utils\SmartcardService;
 
 class VoucherController extends Controller
 {
+    /** @var SerializerInterface */
+    private $serializer;
+
+    /** @var ValidatorInterface */
+    private $validator;
+
+    /** @var LoggerInterface */
+    private $logger;
+
+    /** @var PurchaseService */
+    private $purchaseService;
+
+    public function __construct(
+        SerializerInterface $serializer,
+        ValidatorInterface $validator,
+        LoggerInterface $logger,
+        PurchaseService $purchaseService
+    ) {
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+        $this->logger = $logger;
+        $this->purchaseService = $purchaseService;
+    }
+
     /**
      * Provide purchase of goods for vouchers.
      * If vendor scan some vouchers and sell some goods for them, this request will send.
@@ -26,27 +56,27 @@ class VoucherController extends Controller
      */
     public function purchase(Request $request)
     {
-        $data = $this->get('serializer')->deserialize($request->getContent(), VoucherPurchase::class . '[]', 'json');
+        $data = $this->serializer->deserialize($request->getContent(), VoucherPurchase::class . '[]', 'json');
 
-        $errors = $this->get('validator')->validate($data, [
+        $errors = $this->validator->validate($data, [
             new All([new Type(['type' => VoucherPurchase::class])]),
             new Valid(),
         ]);
 
         if (count($errors) > 0) {
-            $this->container->get('logger')->error('validation errors: ' . ((string) $errors));
+            $this->logger->error('validation errors: ' . ((string) $errors));
 
             return new Response((string) $errors, Response::HTTP_BAD_REQUEST);
         }
 
         try {
             foreach ($data as $item) {
-                $this->get('voucher.purchase_service')->purchase($item);
+                $this->purchaseService->purchase($item);
             }
 
             return new Response(json_encode(true));
         } catch (EntityNotFoundException $ex) {
-            $this->container->get('logger')->error('Entity not found: ', [$ex->getMessage()]);
+            $this->logger->error('Entity not found: ', [$ex->getMessage()]);
 
             return new Response($ex->getMessage(), Response::HTTP_BAD_REQUEST);
         }
