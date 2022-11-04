f4c243f790f5808431b538338f70420c3adaf529
Vladimír Blažek <vladimir.blazek@quanti.cz>
2022-11-04 10:52:21 +0100

    [PIN-4094] Remove nelmio-cors and add own implementation

--- src/Listener/ResponseEventSubscriber.php
+++ src/Listener/ResponseEventSubscriber.php
@@ -23,80 +23,83 @@ class ResponseEventSubscriber implements \Symfony\Component\EventDispatcher\Even
 
     public function onKernelResponse(ResponseEvent $event)
     {
-        //$this->em->getUnitOfWork()->computeChangeSets();
-        //var_dump(get_class(current($this->em->getUnitOfWork()->getScheduledEntityUpdates())));
-        //die();
+        if (strtoupper($event->getRequest()->getRealMethod()) !== 'OPTIONS') {
+            //$this->em->getUnitOfWork()->computeChangeSets();
+            //var_dump(get_class(current($this->em->getUnitOfWork()->getScheduledEntityUpdates())));
+            //die();
 
-        //$this->em->flush();
-        //die();
+            //$this->em->flush();
+            //die();
 
-        $response = $event->getResponse();
-        $request = $event->getRequest();
-        $user = $this->getUser();
+            $response = $event->getResponse();
+            $request = $event->getRequest();
+            $user = $this->getUser();
 
-        //Uid
-        $idUser = $user['id'];
-        //Umail
-        $mailUser = $user['email'];
-        //Controller
-        $controller = $request->attributes->get('_controller');
-        //url
-        $url = $request->getPathInfo();
-        //Date
-        $date = new DateTime('now');
-        //Method
-        $method = $request->getMethod();
-        //HTTPStatus
-        $httpStatus = $response->getStatusCode();
-        //Request
-        $requestAll = $request->request->all();
+            //Uid
+            $idUser = $user['id'];
+            //Umail
+            $mailUser = $user['email'];
+            //Controller
+            $controller = $request->attributes->get('_controller');
+            //url
+            $url = $request->getPathInfo();
+            //Date
+            $date = new DateTime('now');
+            //Method
+            $method = $request->getMethod();
+            //HTTPStatus
+            $httpStatus = $response->getStatusCode();
+            //Request
+            $requestAll = $request->request->all();
 
-        //do not log any login requests
-        //TODO this is nasty... whole mechanism of logging to "log" table should be rewritten or abandoned
-        if (str_contains($url, 'login')) {
-            $requestAll = '';
-        }
-
-        //Fake POST urls
-        $isFakePost = preg_match('/.*\/households\/get\/.*/', $url) ||
-            preg_match('/.*\/export/', $url) ||
-            preg_match('/.*\/location\/.+/', $url) ||
-            preg_match('/.*\/distributions\/criteria\/project\/\d+\/number/', $url) ||
-            preg_match('/.*\/distributions\/beneficiaries\/project\/\d+/', $url) ||
-            preg_match('/.*\/indicators/', $url) ||
-            preg_match('/.*\/login.+/', $url) ||
-            preg_match('/.*\/booklets-print/', $url) ||
-            // Unused until the App is fixed to not send a request each time it syncs;
-            preg_match('/.*\/vouchers\/scanned/', $url) ||
-            preg_match('/.*\/deactivate-booklets/', $url);
-
-        if (
-            $idUser && $method != 'GET' && explode(
-                '\\',
-                (string) $controller
-            )[0] != "ReportingBundle" && (!$isFakePost || $method !== 'POST')
-        ) {
-            $log = new Logs();
-
-            $log->setUrl($url)
-                ->setIdUser('5')
-                ->setMailUser('yo')
-                ->setMethod($method)
-                ->setDate($date)
-                ->setHttpStatus($httpStatus)
-                ->setController($controller)
-                ->setRequest(json_encode($requestAll, JSON_THROW_ON_ERROR));
-
-            if (!$this->em->isOpen()) {
-                $this->em = $this->em->create(
-                    $this->em->getConnection(),
-                    $this->em->getConfiguration()
-                );
+            //do not log any login requests
+            //TODO this is nasty... whole mechanism of logging to "log" table should be rewritten or abandoned
+            if (str_contains($url, 'login')) {
+                $requestAll = '';
             }
 
-            $this->em->persist($log);
-            $this->em->flush();
+            //Fake POST urls
+            $isFakePost = preg_match('/.*\/households\/get\/.*/', $url) ||
+                preg_match('/.*\/export/', $url) ||
+                preg_match('/.*\/location\/.+/', $url) ||
+                preg_match('/.*\/distributions\/criteria\/project\/\d+\/number/', $url) ||
+                preg_match('/.*\/distributions\/beneficiaries\/project\/\d+/', $url) ||
+                preg_match('/.*\/indicators/', $url) ||
+                preg_match('/.*\/login.+/', $url) ||
+                preg_match('/.*\/booklets-print/', $url) ||
+                // Unused until the App is fixed to not send a request each time it syncs;
+                preg_match('/.*\/vouchers\/scanned/', $url) ||
+                preg_match('/.*\/deactivate-booklets/', $url);
+
+            if (
+                $idUser && $method != 'GET' && explode(
+                    '\\',
+                    (string) $controller
+                )[0] != "ReportingBundle" && (!$isFakePost || $method !== 'POST')
+            ) {
+                $log = new Logs();
+
+                $log->setUrl($url)
+                    ->setIdUser('5')
+                    ->setMailUser('yo')
+                    ->setMethod($method)
+                    ->setDate($date)
+                    ->setHttpStatus($httpStatus)
+                    ->setController($controller)
+                    ->setRequest(json_encode($requestAll, JSON_THROW_ON_ERROR));
+
+                if (!$this->em->isOpen()) {
+                    $this->em = $this->em->create(
+                        $this->em->getConnection(),
+                        $this->em->getConfiguration()
+                    );
+                }
+
+                $this->em->persist($log);
+                $this->em->flush();
+            }
         }
+
     }
 
     /**
