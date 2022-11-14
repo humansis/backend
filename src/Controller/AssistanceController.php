ec1132f76ccba261dda30a8abae96cbc78e63ab0
Lukas Kral <luke.kral@seznam.cz>
2022-11-14 10:32:28 +0100

    cs

--- src/Controller/AssistanceController.php
+++ src/Controller/AssistanceController.php
@@ -40,7 +40,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Mime\FileinfoMimeTypeGuesser;
 use Component\Assistance\Domain\Assistance as DomainAssistance;
 
