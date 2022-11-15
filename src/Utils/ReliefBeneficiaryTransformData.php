ce22907a2f83f3895324d20babdf31a6b5ce282c
Abdulrahman Alamouri <alaabd01@pinf.cz>
2022-11-15 11:48:43 +0300

    [PIN-4203] Rewrite export - Assistance list of beneficiaries

---
+++ src/Utils/ReliefBeneficiaryTransformData.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Utils;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ReliefBeneficiaryTransformData
+{
+    /** @var TranslatorInterface */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * Returns an array representation of relief beneficiaries in order to prepare the export
+     *
+     * @param $packages
+     *
+     * @return array
+     */
+    public function transformData($packages): array
+    {
+        $exportableTable = [];
+        foreach ($packages as $relief) {
+            $beneficiary = $relief->getAssistanceBeneficiary()->getBeneficiary();
+            $commodityNames = $relief->getModalityType();
+            $commodityValues = $relief->getAmountToDistribute() . ' ' . $relief->getUnit();
+
+            $commonFields = $beneficiary->getCommonExportFields();
+
+            $exportableTable[] = array_merge($commonFields, [
+                $this->translator->trans("Commodity") => $commodityNames,
+                $this->translator->trans("Value") => $commodityValues,
+                $this->translator->trans("Distributed At") => $relief->getLastModifiedAt(),
+                $this->translator->trans("Notes Distribution") => $relief->getNotes(),
+                $this->translator->trans("Removed") => $relief->getAssistanceBeneficiary()->getRemoved() ? 'Yes' : 'No',
+                $this->translator->trans("Justification for adding/removing") => $relief->getAssistanceBeneficiary()->getJustification(),
+            ]);
+        }
+        return $exportableTable;
+    }
+}
