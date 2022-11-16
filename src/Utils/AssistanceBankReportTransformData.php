4cc4b82503ff520c3faa61bc89950f411b291c96
Abdulrahman Alamouri <alaabd01@pinf.cz>
2022-11-16 11:18:58 +0300

    [PIN-4202] Rewrite export - Assistance bank distribution list

---
+++ src/Utils/AssistanceBankReportTransformData.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utils;
+
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class AssistanceBankReportTransformData
+{
+    /** @var TranslatorInterface */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * Returns an array representation of assistance bank report in order to prepare the export
+     *
+     * @param $distributions
+     *
+     * @return array
+     */
+    public function transformData($distributions): array
+    {
+        $exportableTable = [];
+
+        foreach ($distributions as $distribution) {
+            $exportableTable [] = [
+                $this->translator->trans('Ordinal number') => $distribution['distributionId'],
+                $this->translator->trans('Recipient’s surname (Local family name)') => $distribution['localFamilyName'],
+                $this->translator->trans('Recipient’s name (Local given name)') => $distribution['localGivenName'],
+                $this->translator->trans('Recipient’s patronymic (Local parent’s name)') => $distribution['localParentsName'],
+                $this->translator->trans('Recipient’s RNTRC (Tax Number)') => $distribution['idNumber'],
+                $this->translator->trans('Document type') => $distribution['countrySpecificValue1'],
+                $this->translator->trans('Document number') => $distribution['countrySpecificValue2'],
+                $this->translator->trans('Remittance purpose') => 'Благодійна допомога',
+                $this->translator->trans('Remittance amount') => $distribution['amountToDistribute'],
+                $this->translator->trans('Remittance currency') => $distribution['currency'],
+                $this->translator->trans('Recipient’s mobile telephone number') => $distribution['phoneNumber']
+            ];
+        }
+        return $exportableTable;
+    }
+}
