f4c834ea4e207229a70746d9942b3ef5dd39bdf7
David Slacalek <david.slacalek@quanti.cz>
2022-11-24 13:56:49 +0100

    [PIN-3886] sentry dns into config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -50,6 +50,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
             new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
             new DHAuditorBundle(),
+            new Sentry\SentryBundle\SentryBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['local', 'dev', 'test'], true)) {
@@ -60,10 +61,6 @@ class AppKernel extends Kernel
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
             }
-        }
-
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
-            $bundles[] = new Sentry\SentryBundle\SentryBundle();
         }
 
         return $bundles;
