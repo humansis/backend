f07291b6c6f455a53e6e5482604df443abe16de9
Lukas Kral <luke.kral@seznam.cz>
2022-12-12 15:28:40 +0100

    [PIN-4428]: CS

--- src/Component/Import/Enum/ImportCsoEnum.php
+++ src/Component/Import/Enum/ImportCsoEnum.php
@@ -5,10 +5,8 @@ namespace Component\Import\Enum;
 enum ImportCsoEnum: string
 {
     case MappingKey = 'countrySpecifics';
-    case CsoColumnPath =  'CountrySpecifics';
-
+    case CsoColumnPath = 'CountrySpecifics';
     case ImportLineEntityKey = 'countrySpecific';
-
     case ImportLineValueKey = 'value';
 
     public static function getCsoColumnMapping(string $column): string
