8ae5717e426fcf9e225316f6f72233ba9737cb9a
Abdulrahman Alamouri <alaabd01@pinf.cz>
2023-01-10 09:34:46 +0300

    [PIN-3831] cleaning the code

--- src/Controller/OfflineApp/ProjectController.php
+++ src/Controller/OfflineApp/ProjectController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Controller\OfflineApp;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Mapper\ProjectMapper;
 use Repository\ProjectRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
