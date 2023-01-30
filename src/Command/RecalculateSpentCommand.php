2e10fa1246524669a4d8f647f6f951e1aa0278da
Pavel Kraus <pavel.kraus01@gmail.com>
2023-01-30 01:14:40 +0100

    PIN-4568: Command for recalculating spent column in RP

---
+++ src/Command/RecalculateSpentCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RecalculateSpentCommand extends Command
+{
+    /** @var EntityManagerInterface */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        parent::__construct();
+        $this->em = $em;
+    }
+    protected function configure(): void
+    {
+        parent::configure();
+        $this
+            ->setName('recalculate:spent')
+            ->setDescription('Recalculate column spent in table assistance_relief_package for every row');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Recalculating spent column in assistance_relief_package table');
+
+        $this->em->getConnection()->executeStatement('
+            UPDATE `assistance_relief_package` a
+            JOIN (
+            SELECT
+            arp.id AS aprid,
+            sum(spr.value) AS total
+            FROM assistance a
+
+            -- get beneficiaries
+            JOIN distribution_beneficiary db
+            ON a.id = db.assistance_id
+            JOIN assistance_relief_package arp
+            ON db.id = arp.assistance_beneficiary_id
+            AND arp.modality_type = \'Smartcard\'
+
+            -- filter only smartcard with joined beneficiaries
+            JOIN smartcard_purchase sp
+            ON a.id = sp.assistance_id
+            JOIN smartcard s
+            ON sp.smartcard_id = s.id
+            AND s.beneficiary_id = db.beneficiary_id
+
+            JOIN smartcard_purchase_record spr
+            ON sp.id = spr.smartcard_purchase_id
+
+            GROUP by arp.id
+            ) t ON t.aprid = a.id
+            SET a.amount_spent = t.total
+
+        ');
+
+        $output->writeln('Recalculating spent column in assistance_relief_package table finished');
+
+        return 0;
+    }
+}
